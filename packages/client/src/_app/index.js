2711a7f46b628f594d69fae68026425b1abd22ec
Marwan Zogheib <mzogheib@gmail.com>
2018-01-24 08:02:15 +1100

    [client] add menu placeholder and fill remaining space with map

--- packages/client/src/_app/index.js
+++ packages/client/src/_app/index.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
+import './style.css';
+import Menu from '../menu';
 import Map from '../map';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Map></Map>
+      <div className='app'>
+        <div className='app__menu'><Menu></Menu></div>
+        <div className='app__map'><Map></Map></div>
       </div>
     );
   }
---
+++ packages/client/src/menu/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function Menu(props) {
+  return (
+    <div className='menu'>menu</div>
+  );
+}
+
+export default Menu;
