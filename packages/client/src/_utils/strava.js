1b7ddd3756b6043048c3d319db0d3b1038c560b5
Marwan Zogheib <mzogheib@gmail.com>
2018-02-07 20:20:57 +1100

    [client] add authenticate method to strava module

--- packages/client/src/_utils/strava.js
+++ packages/client/src/_utils/strava.js
@@ -1,7 +1,9 @@
 import Api from './api';
 
+const authenticate = payload => Api.post('strava', payload);
 const getActivities = params => Api.get('strava', params);
 
 export default {
+  authenticate,
   getActivities
 };
