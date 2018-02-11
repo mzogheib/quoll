767eb3745ce0337e7992768f5fd659aa2b385f97
Marwan Zogheib <mzogheib@gmail.com>
2018-02-11 16:57:28 +1100

    [client] display markers instead of heatmaps

--- packages/client/src/map/index.js
+++ packages/client/src/map/index.js
@@ -24,39 +24,32 @@ export default class Map extends React.Component {
   componentWillReceiveProps(nextProps) {
     if (nextProps.layers !== this.props.layers) {
       // Hide existing layers then set to null to delete the layer
-      // TODO: confirm if setting the layer to null in this way is in 
-      // fact deleting it. Are there memory leaks with this approach?
       const currentLayers = this.state.layers;
       currentLayers.forEach(layer => {
-        layer.setMap(null);
-        layer = null;
+        layer.forEach(marker => {
+          marker.setMap(null);
+        });
       });
       this.setState({ layers: [] });
 
       const newLayers = nextProps.layers.map(layer => {
-        return new google.maps.visualization.HeatmapLayer({
-          data: this.makeHeatmapLayerData(layer),
-          radius: 20,
-          opacity: 0.6,
-          maxIntensity: 50
-        });
+        return this.makeMarkers(layer);
       });
       this.setState({ layers: newLayers });
     }
   }
 
-  makeHeatmapLayerData(locations) {
+  makeMarkers(locations) {
     return locations.map(location => {
-      return {
-        location: new google.maps.LatLng(location.latitude, location.longitude),
-        weight: location.total
-      };
-    })
+      return new google.maps.Marker({
+        position: { lat: location.latitude, lng: location.longitude},
+        map: this.state.map,
+        title: location.total
+      });
+    });
   }
 
   render() {
-    this.state.layers.forEach(layer => { layer.setMap(this.state.map); });
-
     return (
       <div className='map' id='map'></div>
     );
