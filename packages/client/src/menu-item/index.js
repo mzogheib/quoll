788735adc93cb1622d20e6775e43cf710f99819c
Marwan Zogheib <mzogheib@gmail.com>
2018-02-18 12:57:16 +1100

    ISSUE #5 [client/menu-item] add a simple expandable detail view

--- packages/client/src/menu-item/index.js
+++ packages/client/src/menu-item/index.js
@@ -1,30 +1,59 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './style.css';
 
-function MenuItem(props) {
-  const renderSummary = () => {
-    if (props.item.connected) {
+class MenuItem extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isExpanded: false
+    };
+  }
+
+  toggleExpand() {
+    this.setState({
+      isExpanded: !this.state.isExpanded
+    });
+  }
+
+  renderSummary = () => {
+    if (this.props.item.connected) {
       return (
         <div className='menu-item__summary-wrapper'>
-          <span className='menu-item__summary'>{props.item.data.length}</span>
-          <button className='menu-item__expand'>More</button>
+          <span className='menu-item__summary'>{this.props.item.data.length}</span>
+          <button className='menu-item__expand' onClick={this.toggleExpand.bind(this)}>{this.state.isExpanded ? 'Less' : 'More'}</button>
         </div>
       );      
     } else {      
       return (
         <div className='menu-item__summary-wrapper'>
-          <a href={props.item.authUrl}>Connect</a>
+          <a href={this.props.item.authUrl}>Connect</a>
         </div>
       );
     }
   }
 
-  return (
-    <div className='menu-item'>
-      <span className='menu-item__name'>{props.item.name}</span>
-      {renderSummary()}
-    </div>
-  );
+  renderDetail() {
+    if (this.state.isExpanded) {
+      return (
+        <div>
+          <div>More Detail</div>
+          <a href={''}>Disconnect</a>
+        </div>
+      );
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <div className='menu-item'>
+          <span className='menu-item__name'>{this.props.item.name}</span>
+          {this.renderSummary()}
+        </div>
+        {this.renderDetail()}
+      </div>
+    );
+  }
 }
 
 export default MenuItem;
