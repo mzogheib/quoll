3163034b985933b20fe495cc84706d3c84e5bf7a
Marwan Zogheib <mzogheib@gmail.com>
2018-02-23 08:30:04 +1100

    ISSUE #5 [client/utils] add a 24 hour time string extractor

--- packages/client/src/_utils/index.js
+++ packages/client/src/_utils/index.js
@@ -1,4 +1,9 @@
-const parseQueryParams = (searchString) => {
+export default {
+    parseQueryParams,
+    extractTimeString
+};
+
+function parseQueryParams (searchString) {
     const queryString = searchString && searchString.split('?') && searchString.split('?')[1];
     if (queryString) {
       const paramPairs = queryString.split('&');
@@ -12,6 +17,19 @@ const parseQueryParams = (searchString) => {
     }
 };
 
-export default {
-    parseQueryParams
-};
+
+// This will extract the first found match regardless of what else is in the input.
+const TIME_FORMAT_REGEX = /[0-9][0-9]:[0-9][0-9]:[0-9][0-9]/;
+function extractTimeString (input) {
+    const match = input.match(TIME_FORMAT_REGEX);
+    var isValid;
+    if (match && match.length) {
+      const timeElements = match[0].split(':');
+      const hour = parseInt(timeElements[0], 10);
+      const minute = parseInt(timeElements[1], 10);
+      const second = parseInt(timeElements[2], 10);
+      isValid = (hour >= 0 && hour < 24) && (minute >= 0 && minute < 60) && (second >= 0 && second < 60);
+    }
+
+    return isValid ? match[0] : null;
+}
