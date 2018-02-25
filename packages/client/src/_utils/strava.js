00b770835a737559600ec46f10fbbf8dc7645e02
Marwan Zogheib <mzogheib@gmail.com>
2018-02-25 16:42:19 +1100

    ISSUE #5 [client/strava] use new strava-auth endpoint

--- packages/client/src/_utils/strava.js
+++ packages/client/src/_utils/strava.js
@@ -2,7 +2,7 @@ import Api from './api';
 import mapUtils from '../map/utils';
 
 const oAuthUrl = 'https://www.strava.com/oauth/authorize?client_id=8709&response_type=code&redirect_uri=http://localhost:3000&state=strava&scope=view_private';
-const authenticate = payload => Api.post('strava', payload);
+const authenticate = payload => Api.post('strava-auth', payload);
 const deauthorize = () => Api.post('strava-deauth');
 const getActivities = params => Api.get('strava', params);
 
