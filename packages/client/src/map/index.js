112ba00a2f9d4c593983aaa3836aff77340f53bc
Marwan Zogheib <mzogheib@gmail.com>
2018-04-22 12:53:13 +1000

    ISSUE #13 [client] update map bounds for each new set of markers and polylines

--- packages/client/src/map/index.js
+++ packages/client/src/map/index.js
@@ -6,19 +6,17 @@ export default class Map extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      map: {},
       layers: []
     };
   }
 
   componentDidMount() {
     const melbourne = new google.maps.LatLng(-37.8079033, 144.9759344);
-    let map = new google.maps.Map(document.getElementById('map'), {
+    this.map = new google.maps.Map(this.refs.map, {
       center: melbourne,
       zoom: 14,
       mapTypeId: 'roadmap',
     });
-    this.setState({ map: map });
   }
 
   componentWillReceiveProps(nextProps) {
@@ -26,25 +24,38 @@ export default class Map extends React.Component {
       // Hide existing layers then set to null to delete the layer
       const currentLayers = this.state.layers;
       currentLayers.forEach(layer => {
-        layer.forEach(marker => {
-          marker.setMap(null);
+        layer.forEach(element => {
+          element.setMap(null);
         });
       });
-      this.setState({ layers: [] });
-
-      const newLayers = nextProps.layers;
-      newLayers.forEach(layer => {
-        layer.forEach(marker => {
-          marker.setMap(this.state.map);
+      this.setState({ layers: [] }, () => {        
+        const newLayers = nextProps.layers;
+        const bounds = new google.maps.LatLngBounds();
+        newLayers.forEach(layer => {
+          layer.forEach(element => {
+            element.setMap(this.map);
+            if (element.getPosition) {
+              // Marker
+              bounds.extend(element.getPosition());
+            } else {
+              // Polyline
+              element.getPath().forEach(position => bounds.extend(position));
+            }
+          });
         });
+  
+        if (!bounds.isEmpty()) {
+          this.map.fitBounds(bounds);
+        }
+  
+        this.setState({ layers: newLayers });
       });
-      this.setState({ layers: newLayers });
     }
   }
 
   render() {
     return (
-      <div className='map' id='map'></div>
+      <div className='map' ref='map'></div>
     );
   }
 };
