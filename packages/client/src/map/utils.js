2af7141b45c91b7bdbd3f90f8848a7b1e3cf1a49
Marwan Zogheib <mzogheib@gmail.com>
2018-04-29 15:16:34 +1000

    ISSUE #11 [client] increase polyline weight

--- packages/client/src/map/utils.js
+++ packages/client/src/map/utils.js
@@ -23,7 +23,7 @@ function makePolyline({ encodedPath, map }) {
     geodesic: true,
     strokeColor: '#FF0000',
     strokeOpacity: 1.0,
-    strokeWeight: 2
+    strokeWeight: 5
   };
   if (map) polyline.map = map;
   return new google.maps.Polyline(polyline);
