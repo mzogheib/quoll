844759167a214c5c1a1393320ec7962ba4b7ade2
Marwan Zogheib <mzogheib@gmail.com>
2018-05-10 10:51:24 +1000

    [server] refactor strava api

--- packages/server/vendor-apis/strava/index.js
+++ packages/server/vendor-apis/strava/index.js
@@ -1,5 +1,5 @@
 const axios = require('axios');
-const utils = require('../../utils');
+const querystring = require('querystring');
 const auth = require('./private/strava-auth');
 
 module.exports = {
@@ -19,44 +19,57 @@ module.exports = {
 const baseUrl = 'https://www.strava.com';
 const baseOauthUrl = `${baseUrl}/oauth`;
 const baseApiUrl = `${baseUrl}/api/v3`;
-const makeAuthHeader = token => { return { headers: { 'Authorization': `Bearer ${token}` } }; };
 
-const get = (url, options) => {
-  return new Promise((resolve, reject) => {
-    axios.get(url, options)
-      .then(response => resolve(response.data))
-      .catch(error => reject({ status: error.response.status, message: error.response.data.message }));
-  });
-}
+const get = ({ baseURL = baseApiUrl, url, headers, params }) => new Promise((resolve, reject) => {
+  const config = {
+    method: 'GET',
+    baseURL,
+    url,
+    headers,
+    params
+  };
+  axios.request(config)
+    .then(response => resolve(response.data))
+    .catch(error => reject({ status: error.response.status, message: error.response.data.message }));
+});
 
-const post = (url, payload) => {
-  return new Promise((resolve, reject) => {
-    axios.post(url, payload)
-      .then(response => resolve(response.data))
-      .catch(error => reject({ status: error.response.status, message: error.response.data.message }));
-  });
-}
+const post = ({ baseURL = baseOauthUrl, url, auth, headers, data }) => new Promise((resolve, reject) => {
+  const config = {
+    method: 'POST',
+    baseURL,
+    url,
+    auth,
+    headers,
+    data
+  };
+  axios.request(config)
+    .then(response => resolve(response.data))
+    .catch(error => reject({ status: error.response.status, message: error.response.data.message }));
+});
 
 function oauthUrl () {
-  return `${baseOauthUrl}/authorize?client_id=8709&response_type=code&scope=view_private`;
+  const params = {
+    client_id: '8709',
+    response_type: 'code',
+    scope: 'view_private'
+  };
+  return `${baseOauthUrl}/authorize?${querystring.stringify(params)}`;
 }
 
 function token(code) {
-  const url = `${baseOauthUrl}/token`;
-  const payload = {
+  const data = {
     client_id: auth.client_id,
     client_secret: auth.client_secret,
     code: code
   };
-  return post(url, payload);
+  return post({ url: '/token', data });
 }
 
 function deauthorize(token) {
-  const url = `${baseOauthUrl}/deauthorize`;
-  const payload = {
+  const data = {
     access_token: token
   }
-  return post(url, payload);
+  return post({ url: '/deauthorize', data });
 }
 
 function athleteActivities({ after, before, perPage }, token) {
@@ -65,13 +78,9 @@ function athleteActivities({ after, before, perPage }, token) {
     before,
     per_page: perPage
   };
-  const url = `${baseApiUrl}/athlete/activities${utils.makeUrlParams(params)}`;
-  const options = makeAuthHeader(token);
-  return get(url, options);
+  return get({ url: '/athlete/activities', headers: { Authorization: `Bearer ${token}` }, params });
 }
 
 function getActivity({ id }, token) {
-  const url = `${baseApiUrl}/activities/${id}`;
-  const options = makeAuthHeader(token);
-  return get(url, options);
+  return get({ url: `/activities/${id}`, headers: { Authorization: `Bearer ${token}` }});
 }
