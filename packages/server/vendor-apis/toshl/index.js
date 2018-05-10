522b25d8ef17c553472dee64e8dbd0887dae6a9a
Marwan Zogheib <mzogheib@gmail.com>
2018-05-10 10:05:01 +1000

    [server] refactor toshl api

--- packages/server/vendor-apis/toshl/index.js
+++ packages/server/vendor-apis/toshl/index.js
@@ -19,71 +19,65 @@ module.exports = {
 
 const baseApiUrl = 'https://api.toshl.com';
 const baseOauthUrl = 'https://toshl.com/oauth2';
-const makeBasicAuthHeader = (username, password) => { return { username: auth.client_id, password: auth.client_secret }; };
 
-function get({ baseURL = baseApiUrl, url, headers, params }) {
-  return new Promise((resolve, reject) => {
-    const config = {
-      method: 'GET',
-      baseURL,
-      url,
-      headers,
-      params
-    };
-    axios.request(config)
-      .then(response => resolve(response.data))
-      .catch(error => reject({ status: error.response.status, message: error.response.data.description }));
-  });
-}
+const get = ({ baseURL = baseApiUrl, url, headers, params }) => new Promise((resolve, reject) => {
+  const config = {
+    method: 'GET',
+    baseURL,
+    url,
+    headers,
+    params
+  };
+  axios.request(config)
+    .then(response => resolve(response.data))
+    .catch(error => reject({ status: error.response.status, message: error.response.data.description }));
+});
 
-function post(url, payload, options) {
-  return new Promise((resolve, reject) => {
-    options.headers = options.headers || {};
-    options.headers['Content-Type'] = 'application/x-www-form-urlencoded';
-    return axios.post(url, querystring.stringify(payload), options)
-      .then(response => resolve(response.data))
-      .catch(error => reject({ status: error.response.status, message: error.response.data }));
-  });
-}
+const post = ({ baseURL = baseOauthUrl, url, auth, headers, data }) => new Promise((resolve, reject) => {
+  const config = {
+    method: 'POST',
+    baseURL,
+    url,
+    auth,
+    headers: { ...headers, 'Content-Type': 'application/x-www-form-urlencoded' },
+    data: querystring.stringify(data)
+  };
+  axios.request(config)
+    .then(response => resolve(response.data))
+    .catch(error => reject({ status: error.response.status, message: error.response.data }));
+});
 
 function oauthUrl () {
-  return `${baseOauthUrl}/authorize?client_id=${auth.client_id}&response_type=code&redirect_uri=http://localhost:3000`;
+  const params = {
+    client_id: auth.client_id,
+    response_type: 'code',
+    redirect_uri: 'http://localhost:3000'
+  };
+  return `${baseOauthUrl}/authorize?${querystring.stringify(params)}`;
 }
 
 function token(code) {
-  const url = `${baseOauthUrl}/token`;
-  const payload = {
+  const data = {
     code: code,
     grant_type: 'authorization_code',
     redirect_uri: 'http://localhost:3000'
   };
-  options = {
-    auth: makeBasicAuthHeader(auth.client_id, auth.client_secret)
-  };
-  return post(url, payload, options);
+  return post({ url: '/token', auth: { username: auth.client_id, password: auth.client_secret }, data });
 }
 
 function deauthorize(auth) {
-  const url = `${baseOauthUrl}/revoke`;
-  const payload = {
+  const data = {
     refresh_token: auth.refresh_token
   };
-  const options = {
-    headers: { Authorization: `Bearer ${auth.access_token}` }
-  };
-  return post(url, payload, options);
+  return post({ url: '/revoke', headers: { Authorization: `Bearer ${auth.access_token}` }, data });
 }
 
 function refreshAuth(expiredAuth) {
-  const url = `${baseOauthUrl}/token`;
-  const payload = {
+  const data = {
     grant_type: 'refresh_token',
     refresh_token: expiredAuth.refresh_token
   };
-  options = {
-    auth: makeBasicAuthHeader(auth.client_id, auth.client_secret)
-  };
-  return post(url, payload, options);
+  return post({ url: '/token', auth: { username: auth.client_id, password: auth.client_secret }, data });
 }
 
 function listEntries(params, token) {
