d902a7436dd88255dbafd29a5007099802e479de
Marwan Zogheib <mzogheib@gmail.com>
2018-05-28 22:16:32 +1000

    [client] rename _app to app

--- packages/client/src/index.js
+++ packages/client/src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import './index.css';
-import App from './components/_app';
+import App from './components/app';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(
