563184f86784da8fb03c86a11a48955c862b23e7
Marwan Zogheib <mzogheib@gmail.com>
2018-06-06 19:44:27 +1000

    [client] split root into container and component

--- packages/client/src/containers/root/index.js
+++ packages/client/src/containers/root/index.js
@@ -1,54 +1,6 @@
-import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import App from '../../containers/app';
-import userService from '../../services/user';
 import { setConnectedFeeds } from '../../actions';
-
-class Root extends Component {
-  constructor(props) {
-    super(props);
-    this.renderLoading = this.renderLoading.bind(this);
-    this.renderRoot = this.renderRoot.bind(this);
-    this.state = {
-      isLoading: true
-    }
-  }
-
-  componentDidMount() {
-    const userId = userService.getCurrentUser();
-    const action = userId ? 'login' : 'signup';
-    // TODO: if login fails then clear that user from localStorage and signup
-    userService[action](userId)
-      .then(user => {
-        userService.setCurrentUser(user.id);
-        this.props.setConnectedFeeds(
-          user.feeds.filter(feed => feed.isConnected).map(feed => feed.id)
-        )
-      })
-      .then(() => this.setState({ isLoading: false }));
-  }
-
-  renderLoading() {
-    return (
-      <div>Loading...</div>
-    );
-  }
-
-  renderRoot() {
-    return (
-      <BrowserRouter>
-        <Switch>
-          <Route path="/" component={App} />
-        </Switch>
-      </BrowserRouter>
-    );
-  }
-
-  render() {
-    return this.state.isLoading ? this.renderLoading() : this.renderRoot();
-  }
-}
+import Root from '../../components/root';
 
 const mapDispatchToProps = dispatch => ({
   setConnectedFeeds: ids => dispatch(setConnectedFeeds(ids))
