b3ec40aea167d2394cb849d26b6861af19c7ad7a
Marwan Zogheib <mzogheib@gmail.com>
2018-06-07 22:52:02 +1000

    [client] handle oauth in the settings page

--- packages/client/src/components/app/index.js
+++ packages/client/src/components/app/index.js
@@ -2,23 +2,19 @@ import React, { Component } from 'react';
 import './style.css';
 import Menu from '../menu';
 import Map from '../map';
-import utils from '../../services/utils'
 import feedsService from '../../services/feeds';
 import feedsConfig from '../../services/feeds-config';
-import storageService from '../../services/storage';
 
 class App extends Component {
   constructor(props) {
     super(props);
-    this.handleOAuth = this.handleOAuth.bind(this);
     this.handleFilterUpdate = this.handleFilterUpdate.bind(this);
     this.feedServices = feedsConfig.map(feedsService.make);
   }
 
   componentDidMount() {
     this.refreshFeeds(this.props.feeds, this.props.date)
-      .then(this.props.setFeeds)
-      .then(this.handleOAuth);
+      .then(this.props.setFeeds);
   }
 
   componentWillReceiveProps(nextProps) {
@@ -26,49 +22,6 @@ class App extends Component {
       this.handleFilterUpdate(nextProps.date);
     }
   };
-
-  handleOAuth() {
-    const queryParams = utils.getQueryParams(window.location.href);
-    
-    if (!queryParams || !queryParams.state) {
-      return;
-    } else {
-      // Looks like oauth so remove the query params
-      window.history.replaceState(null, null, window.location.pathname);
-      const oauthState = utils.decode(queryParams.state);
-      const oauthCode = queryParams.code;
-      const oauthError = queryParams.error;
-
-      const feedId = oauthState.id;
-      const feedService = this.feedServices.find(feed => feed.id === feedId);
-      const feed = this.props.feeds.find(feed => feed.id === feedId);
-
-      const token = oauthState.token;
-      const storedToken = storageService.get('oauth-state-token');
-      const tokenIsValid = storedToken && token && storedToken === token;
-      storageService.delete('oauth-state-token');
-
-      if (!feedService || !feed) {
-        alert(`Unknown feed: ${feedId}`);
-        return;
-      } else if (!tokenIsValid || oauthError === 'access_denied') {
-        alert(`${feedService.name} access denied.`);
-        return;
-      } else if (oauthCode) {
-        feedService.authenticate({ code: oauthCode })
-          .then(() => this.refreshFeed(feed, this.props.date))
-          .then(refreshedFeed => ({
-            ...refreshedFeed,
-            isConnected: true
-          }))
-          .then(refreshedFeed => this.props.feeds.map(feed => feed.id === refreshedFeed.id ? refreshedFeed : feed))
-          .then(this.props.setFeeds)
-          .catch(alert)
-        } else {
-          alert(`Unknown response from ${feed.name}.`);
-      }
-    }
-  }
 
   refreshFeeds(feeds, date) {
     const promises = feeds.map(feed => feed.isConnected ? this.refreshFeed(feed, date) : Promise.resolve(feed))
--- packages/client/src/components/settings/index.js
+++ packages/client/src/components/settings/index.js
@@ -8,6 +8,50 @@ import storageService from '../../services/storage';
 function Settings(props) {
   const feedServices = feedsConfig.map(feedsService.make);
 
+  handleOAuth()
+
+  function handleOAuth() {
+    const queryParams = utils.getQueryParams(window.location.href);
+    
+    if (!queryParams || !queryParams.state) {
+      return;
+    } else {
+      // Looks like oauth so remove the query params
+      window.history.replaceState(null, null, window.location.pathname);
+      const oauthState = utils.decode(queryParams.state);
+      const oauthCode = queryParams.code;
+      const oauthError = queryParams.error;
+
+      const feedId = oauthState.id;
+      const feedService = feedServices.find(feed => feed.id === feedId);
+      const feed = props.feeds.find(feed => feed.id === feedId);
+
+      const token = oauthState.token;
+      const storedToken = storageService.get('oauth-state-token');
+      const tokenIsValid = storedToken && token && storedToken === token;
+      storageService.delete('oauth-state-token');
+
+      if (!feedService || !feed) {
+        alert(`Unknown feed: ${feedId}`);
+        return;
+      } else if (!tokenIsValid || oauthError === 'access_denied') {
+        alert(`${feedService.name} access denied.`);
+        return;
+      } else if (oauthCode) {
+        feedService.authenticate({ code: oauthCode })
+          .then(() => ({
+            ...feed,
+            isConnected: true
+          }))
+          .then(connectedFeed => props.feeds.map(feed => feed.id === connectedFeed.id ? connectedFeed : feed))
+          .then(props.setFeeds)
+          .catch(alert)
+        } else {
+          alert(`Unknown response from ${feed.name}.`);
+      }
+    }
+  }
+
   function connectFeed(id) {
     const feedService = feedServices.find(feed => feed.id === id);
     const token = utils.makeRandomString();
@@ -28,6 +72,8 @@ function Settings(props) {
           isConnected: false,
           summary: 'None',
           summaryList: [],
+          data: [],
+          mapData: []
         };
         const updatedFeeds = props.feeds.map(feed => feed.id === disconnectedFeed.id ? disconnectedFeed : feed);
         props.setFeeds(updatedFeeds)
