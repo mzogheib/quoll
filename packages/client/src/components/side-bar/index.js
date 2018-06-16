116a04037fac068d84c1e12f7a01c54a15c9ac14
Marwan Zogheib <mzogheib@gmail.com>
2018-06-16 22:45:51 +1000

    ISSUE #19 [client] add map and settings icons using react-icons

--- packages/client/src/components/side-bar/index.js
+++ packages/client/src/components/side-bar/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 import './style.css';
+import MapIcon from 'react-icons/lib/md/map';
+import SettingsIcon from 'react-icons/lib/md/settings';
 
 function SideBar() {
   return (
@@ -9,13 +11,13 @@ function SideBar() {
       <div className="side-bar__nav">
         <div className="side-bar__nav-upper">
           <NavLink to={'/'} exact className="side-bar__link" activeClassName="side-bar__link-active">
-            <div className="side-bar__link-icon">Map</div>
+            <div className="side-bar__link-icon"><MapIcon size={40}/></div>
             <div className="side-bar__link-active-indicator"></div>
           </NavLink>
         </div>
         <div className="side-bar__nav-lower">
           <NavLink to={'/settings'} className="side-bar__link" activeClassName="side-bar__link-active">
-            <div className="side-bar__link-icon">Settings</div>
+            <div className="side-bar__link-icon"><SettingsIcon size={40}/></div>
             <div className="side-bar__link-active-indicator"></div>
           </NavLink>
         </div>
