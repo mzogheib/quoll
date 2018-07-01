fd7123e7131a8ec06b611978cc76d69b57a104c9
Marwan Zogheib <mzogheib@gmail.com>
2018-07-01 15:53:47 +1000

    [client] move date-picker and feed-entries up into the home route

--- packages/client/src/routes/home/component.js
+++ packages/client/src/routes/home/component.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './style.css';
-import Feed from '../../components/feed';
+import DatePicker from '../../components/date-picker';
+import FeedEntries from '../../components/feed-entries';
 import Map from '../../components/map';
 import Loader from '../../components/horizontal-loader';
 
@@ -13,7 +14,12 @@ class App extends Component {
   render() {
     return (
       <div className='home'>
-        <div className='home__feed'><Feed /></div>
+        <div className='home__feed'>
+          <DatePicker />
+          <div className='home__feed-entries-wrapper'>
+            <div className='home__feed-entries'><FeedEntries /></div>
+          </div>
+        </div>
         <div className='home__map-wrapper'>
           <div className='home__map'><Map /></div>
         </div>
