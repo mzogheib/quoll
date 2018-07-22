f82202fb1a64003010461eb9c81ef9c42eefe265
Marwan Zogheib <mzogheib@gmail.com>
2018-07-22 15:46:42 +1000

    ISSUE #24 [client] remove unused feed service code

--- packages/client/src/services/feeds/index.js
+++ packages/client/src/services/feeds/index.js
@@ -1,13 +1,42 @@
-export default {
-  make
-};
+import toshl from './toshl';
+import strava from './strava';
+import moves from './moves';
 
-function make({ id, name, getOauthUrl, authenticate, disconnect }) {
-  return {
-    id,
-    name,
-    getOauthUrl,
-    authenticate,
-    disconnect
-  };
-};
+export default [
+  {
+    id: 'toshl',
+    name: 'Toshl',
+    link: {
+      url: 'https://toshl.com',
+      label: 'toshl.com'
+    },
+    image: require('../images/toshl-196x196.png'),
+    getOauthUrl: toshl.getOauthUrl,
+    authenticate: toshl.authenticate,
+    disconnect: toshl.deauthorize,
+  },
+  {
+    id: 'strava',
+    name: 'Strava',
+    link: {
+      url: 'https://www.strava.com',
+      label: 'www.strava.com'
+    },
+    image: require('../images/strava-96x96.png'),
+    getOauthUrl: strava.getOauthUrl,
+    authenticate: strava.authenticate,
+    disconnect: strava.deauthorize,
+  },
+  {
+    id: 'moves',
+    name: 'Moves',
+    link: {
+      url: 'https://www.moves-app.com',
+      label: 'www.moves-app.com'
+    },
+    image: require('../images/moves-206x206.png'),
+    getOauthUrl: moves.getOauthUrl,
+    authenticate: moves.authenticate,
+    disconnect: moves.deauthorize,
+  }
+];
--- packages/client/src/store/feeds/index.js
+++ packages/client/src/store/feeds/index.js
@@ -1,12 +1,4 @@
-import feedsConfig from '../../services/feeds/config';
-import feedsService from '../../services/feeds';
-
-const feedServices = feedsConfig.map(feedsService.make);
-
-export const setFeeds = feeds => ({
-  type: 'SET_FEEDS',
-  feeds
-});
+import feedServices from '../../services/feeds';
 
 export const setConnectedFeeds = ids => ({
   type: 'SET_CONNECTED_FEEDS',
@@ -16,12 +8,6 @@ export const setConnectedFeeds = ids => ({
 export const setDisconnectedFeeds = ids => ({
   type: 'SET_DISCONNECTED_FEEDS',
   ids
-});
-
-export const setFeedFetching = (id, value) => ({
-  type: 'SET_FEED_FETCHING',
-  id,
-  value
 });
 
 export const setFeedAuthenticating = (id, value) => ({
@@ -63,66 +49,23 @@ export const disconnectFeed = (id) => {
   };
 };
 
-const refreshFeed = (feed, date) => {
-  const feedService = feedServices.find(feedService => feedService.id === feed.id);
-  return (dispatch) => {
-    dispatch(setFeedFetching(feed.id, true))
-    return feedService.getData(date).then(data => {
-      dispatch(setFeedFetching(feed.id, false))
-      return {
-        ...feed,
-        data: data,
-        entries: feedService.makeEntries(data),
-        mapData: feedService.makeMapData(data)
-      };
-    })
-  }
-}
-
-export const refreshFeeds = () => {
-  return (dispatch, getState) => {
-    const state = getState();
-    const date = state.date;
-    const promises = state.feeds.map(feed => feed.isConnected ? dispatch(refreshFeed(feed, date)) : Promise.resolve(feed))
-    return Promise.all(promises).then(feeds => dispatch(setFeeds(feeds)));
-  };
-};
-
-
 // TODO: entries can have its own reducer. Can also add map data too
 // See how it's done here under 'Handling Actions', https://redux.js.org/advanced/async-actions
-const defaultFeeds = feedsConfig.map(config => ({
+const defaultFeeds = feedServices.map(config => ({
   id: config.id,
   name: config.name,
   link: config.link,
   image: config.image,
-  isFetching: false,
   isConnected: false,
   isAuthenticating: false,
-  data: [],
-  entries: [],
-  mapData: [],
 }));
 
 const feeds = (state = defaultFeeds, action) => {
   switch (action.type) {
-    case 'SET_FEEDS':
-      return action.feeds;
     case 'SET_CONNECTED_FEEDS':
       return state.map(feed => action.ids.includes(feed.id) ? { ...feed, isConnected: true } : feed)
     case 'SET_DISCONNECTED_FEEDS':
-      return state.map(feed => action.ids.includes(feed.id) ?
-        {
-          ...feed,
-          isConnected: false,
-          entries: [],
-          data: [],
-          mapData: []
-        } :
-        feed
-      )
-    case 'SET_FEED_FETCHING':
-      return state.map(feed => feed.id === action.id ? { ...feed, isFetching: action.value } : feed)
+      return state.map(feed => action.ids.includes(feed.id) ? { ...feed, isConnected: false } : feed )
     case 'SET_FEED_AUTHENTICATING':
       return state.map(feed => feed.id === action.id ? { ...feed, isAuthenticating: action.value } : feed)
     default:
