a6b3f59e0f7cddcfa592178ae8751015347d3182
Marwan Zogheib <mzogheib@gmail.com>
2018-07-23 19:34:44 +1000

    ISSUE #24 [server] remove old feed route code

--- packages/server/routes/feed.route.js
+++ packages/server/routes/feed.route.js
@@ -1,46 +1,11 @@
-const serviceMoves = require('../services/moves');
-const serviceStrava = require('../services/strava');
-const serviceToshl = require('../services/toshl');
 const ctrlUsers = require('../controllers/users.controller');
 const ctrlFeed = require('../controllers/feed.controller');
-
-const getMethods = {
-  moves: serviceMoves.getSegments,
-  strava: serviceStrava.getAthleteActivities,
-  toshl: serviceToshl.getEntries
-}
 
 module.exports = {
   get
 };
 
-function get(req, res) {
-  const { source, from, to } = req.query;
-  const { userId } = req;
-
-  const respond = ({ status, message }) => res.status(status).json(message);
-  const onSuccess = data => respond({ status: 200, message: data });
-  const onError = error => respond({ status: error.status || 500, message: error.message });
-
-  if (!source) {
-    // respond({ status: 400, message: 'No feed source provided.' });
-    getSingle(req, res);
-  } else if (!from || !to) {
-    respond({ status: 400, message: 'Invalid params provided.' });
-  } else {
-    const getMethod = getMethods[source];
-    if (!getMethod) {
-      respond({ status: 404, message: `Unkown feed source: ${source}` });
-    } else {
-      ctrlUsers.getVendorAuth(userId, source)
-        .then(auth => getMethod(from, to, auth.access_token))
-        .then(onSuccess)
-        .catch(onError);
-    }
-  }
-}
-
-function getSingle (req, res) {
+function get (req, res) {
   const { from, to } = req.query;
   const { userId } = req;
 
