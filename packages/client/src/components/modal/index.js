3b0bde0368f3310fc5844a8191bb11b50fb0ea4f
Marwan Zogheib <mzogheib@gmail.com>
2019-05-18 15:18:37 +1000

    ISSUE #33 [client] Replace custom modal with react-modal

--- packages/client/src/components/modal/index.js
+++ packages/client/src/components/modal/index.js
@@ -1,4 +1,42 @@
 import React from 'react'
-import './style.scss'
+import ReactModal from 'react-modal'
 
-export default ({ children }) => <div className="modal">{children}</div>
+ReactModal.setAppElement('#root')
+
+export default ({ isOpen, onRequestClose, children, maxWidth }) => {
+  const customStyle = {
+    overlay: {
+      backgroundColor: 'rgba(196, 196, 196, 0.5)',
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+    // Content will grow as large as the children
+    content: {
+      background: 'transparent',
+      top: 'unset',
+      right: 'unset',
+      bottom: 'unset',
+      left: 'unset',
+      border: 'none',
+      borderRadius: 0,
+      padding: 0,
+      overflow: 'unset',
+      width: '100%',
+      maxWidth,
+    },
+  }
+  return (
+    <ReactModal
+      isOpen={isOpen}
+      onRequestClose={() => {
+        console.log('onRequestClose')
+        onRequestClose()
+      }}
+      shouldCloseOnOverlayClick
+      style={customStyle}
+    >
+      {children}
+    </ReactModal>
+  )
+}
