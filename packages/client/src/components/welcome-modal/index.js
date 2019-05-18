65d68d8c69e32d50ae6d542e2026171483a7977f
Marwan Zogheib <mzogheib@gmail.com>
2019-05-18 16:07:39 +1000

    ISSUE #33 [client] simplify welcome modal render

--- packages/client/src/components/welcome-modal/index.js
+++ packages/client/src/components/welcome-modal/index.js
@@ -3,31 +3,23 @@ import './style.scss'
 import Modal from '../modal'
 import CloseIcon from 'react-icons/lib/md/close'
 
-const WelcomeModal = ({ isOpen, onCancel, onConnect }) => {
-  const renderContent = () => (
-    <div>
-      <div className="welcome-modal__header">
-        <CloseIcon
-          className="welcome-modal__cancel"
-          size={30}
-          onClick={onCancel}
-        />
-      </div>
-      <div className="welcome-modal__title">Quoll</div>
-      <div className="welcome-modal__message">Map yo' life.</div>
-      <div className="welcome-modal__actions">
-        <button className="welcome-modal__actions-connect" onClick={onConnect}>
-          Connect Feeds
-        </button>
-      </div>
+const WelcomeModal = ({ isOpen, onCancel, onConnect }) => (
+  <Modal className="welcome-modal" isOpen={isOpen} onRequestClose={onCancel}>
+    <div className="welcome-modal__header">
+      <CloseIcon
+        className="welcome-modal__cancel"
+        size={30}
+        onClick={onCancel}
+      />
     </div>
-  )
-
-  return (
-    <Modal className="welcome-modal" isOpen={isOpen} onRequestClose={onCancel}>
-      {renderContent()}
-    </Modal>
-  )
-}
+    <div className="welcome-modal__title">Quoll</div>
+    <div className="welcome-modal__message">Map yo' life.</div>
+    <div className="welcome-modal__actions">
+      <button className="welcome-modal__actions-connect" onClick={onConnect}>
+        Connect Feeds
+      </button>
+    </div>
+  </Modal>
+)
 
 export default WelcomeModal
