f64a4bfed98fdcf6c9304121ab737fc1411da921
Marwan Zogheib <mzogheib@gmail.com>
2019-05-24 21:13:00 +1000

    ISSUE #38 [client] make a plain button and use in FeedSettings

--- packages/client/src/components/Button/index.js
+++ packages/client/src/components/Button/index.js
@@ -1,15 +1,55 @@
 import React from 'react'
 import './style.scss'
 
-const renderBase = ({ className = 'default-button', label, onClick }) => (
-  <button className={className} onClick={onClick}>
+// Filter out falsey values
+const joinClassNames = classNameArray => classNameArray.filter(c => c).join(' ')
+
+const classMap = {
+  default: 'default-button',
+  primary: 'primary-button',
+  plain: 'plain-button',
+}
+
+const hitboxClassMap = {
+  small: '--hitbox-small',
+  none: '--hitbox-none',
+}
+
+const renderBase = ({
+  variant = 'default',
+  label,
+  onClick,
+  disabled,
+  bold,
+  hitboxSize,
+}) => (
+  <button
+    className={joinClassNames([
+      classMap[variant],
+      disabled && '--disabled',
+      bold && '--bold',
+      hitboxClassMap[hitboxSize],
+    ])}
+    onClick={onClick}
+    disabled={disabled}
+    type="button"
+  >
     {label}
   </button>
 )
 
 const Button = props => renderBase(props)
 
-Button.Primary = props => renderBase({ className: 'primary-button', ...props })
+Button.Primary = props => renderBase({ variant: 'primary', ...props })
 Button.Primary.displayName = 'PrimaryButton'
+
+Button.Plain = props =>
+  renderBase({
+    variant: 'plain',
+    bold: true,
+    hitboxSize: 'none',
+    ...props,
+  })
+Button.Plain.displayName = 'PlainButton'
 
 export default Button
