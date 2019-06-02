b1df2c1a0513a84e0fb2c039301e0055b57e6f17
Marwan Zogheib <mzogheib@gmail.com>
2019-06-02 22:52:08 +1000

    ISSUE #40 [client] refactor Button

--- packages/client/src/components/Button/index.js
+++ packages/client/src/components/Button/index.js
@@ -2,51 +2,26 @@ import styled, { css } from 'styled-components'
 import { lighten, darken } from 'polished'
 import PropTypes from 'prop-types'
 
-const disabledRules = (disabled, backgroundColor, color) => {
-  if (!disabled) return
-
-  return css`
-    background-color: ${lighten(0.1, backgroundColor)};
-    color: ${lighten(0.4, color)};
-
-    &:hover {
-      cursor: unset;
-      background-color: ${lighten(0.1, backgroundColor)};
-    }
-  `
-}
-
-const hitboxRules = noHitbox => {
-  if (!noHitbox) return
-
-  return css`
-    height: unset;
-    min-width: unset;
-    padding: 0;
-  `
-}
-
 const makeStyle = ({ color, backgroundColor, bold, disabled, noHitbox }) => css`
   border: none;
   border-radius: 4px;
 
-  height: 36px;
-  min-width: 120px;
-  background-color: ${backgroundColor};
-  padding: 0 15px;
-
-  color: ${color};
   font-size: 14px;
   font-weight: ${bold ? 500 : null};
 
+  height: ${noHitbox ? 'unset' : '36px'};
+  min-width: ${noHitbox ? 'unset' : '120px'};
+  padding: ${noHitbox ? 0 : '0 15px'};
+
+  background-color: ${disabled
+    ? lighten(0.1, backgroundColor)
+    : backgroundColor};
+  color: ${disabled ? lighten(0.4, color) : color};
+  cursor: ${disabled ? 'unset' : 'pointer'};
+
   &:hover {
-    cursor: pointer;
-    background-color: ${darken(0.05, backgroundColor)};
+    background-color: ${disabled ? 'initial' : darken(0.05, backgroundColor)};
   }
-
-  ${disabledRules(disabled, backgroundColor, color)};
-
-  ${hitboxRules(noHitbox)};
 `
 
 const defaultStyle = ({ theme: { colors }, disabled }) => css`
@@ -91,21 +66,21 @@ const defaultProps = {
 // This avoids classes that have multiple overrides. On the other hand,
 // it ends up creating three sets of distinct styles.
 const Button = {
-  ...styled.button([defaultStyle]),
+  ...styled.button(defaultStyle),
   displayName: 'Button',
   propTypes,
   defaultProps,
 }
 
 Button.Primary = {
-  ...styled.button([primaryStyle]),
+  ...styled.button(primaryStyle),
   displayName: 'Button.Primary',
   propTypes,
   defaultProps,
 }
 
 Button.Plain = {
-  ...styled.button([plainStyle]),
+  ...styled.button(plainStyle),
   displayName: 'Button.Plain',
   propTypes,
   defaultProps,
