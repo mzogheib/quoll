3e50c917b8634700a794c0be3d9fecd8725c62b4
Marwan Zogheib <mzogheib@gmail.com>
2019-06-05 22:38:21 +1000

    ISSUE #40 [client] migrate top level styles to be defined in createGlobalStyle

--- packages/client/src/index.js
+++ packages/client/src/index.js
@@ -1,20 +1,40 @@
-import React from 'react'
-import { ThemeProvider } from 'styled-components'
+import React, { Fragment } from 'react'
+import { createGlobalStyle, ThemeProvider } from 'styled-components'
 import { render } from 'react-dom'
 import { Provider } from 'react-redux'
 import { BrowserRouter } from 'react-router-dom'
-import './index.scss'
 import 'typeface-pacifico'
 import registerServiceWorker from './registerServiceWorker'
 import App from './App'
 import store from './store'
 import themes from './themes'
 
+const GlobalStyle = createGlobalStyle`
+  html,
+  body,
+  .rootdiv {
+    height: 100%;
+  }
+
+  body {
+    margin: 0;
+    padding: 0;
+    font-family: Roboto, sans-serif;
+  }
+
+  * {
+    box-sizing: border-box;
+  }
+`
+
 render(
   <Provider store={store}>
     <BrowserRouter>
       <ThemeProvider theme={themes.default}>
-        <App />
+        <Fragment>
+          <App />
+          <GlobalStyle />
+        </Fragment>
       </ThemeProvider>
     </BrowserRouter>
   </Provider>,
