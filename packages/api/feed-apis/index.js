b88a980e65bb5fedfec295435ec1cbe1dff9be45
Marwan Zogheib <mzogheib@gmail.com>
2019-07-07 22:44:40 +1000

    ISSUE #28 [api] move 3rd party credentials to environment variables

--- packages/api/feed-apis/index.js
+++ packages/api/feed-apis/index.js
@@ -1,14 +1,43 @@
 // This is probably trying to be more clever than it's worth but it removes a lot of boilerplate
-const apis = ['moves', 'strava', 'toshl', 'uber']
+const apis = [
+  {
+    name: 'moves',
+    auth: {
+      client_id: process.env.MOVES_CLIENT_ID,
+      client_secret: process.env.MOVES_CLIENT_SECRET,
+    },
+  },
+  {
+    name: 'strava',
+    auth: {
+      client_id: process.env.STRAVA_CLIENT_ID,
+      client_secret: process.env.STRAVA_CLIENT_SECRET,
+    },
+  },
+  {
+    name: 'toshl',
+    auth: {
+      client_id: process.env.TOSHL_CLIENT_ID,
+      client_secret: process.env.TOSHL_CLIENT_SECRET,
+    },
+  },
+  {
+    name: 'uber',
+    auth: {
+      client_id: process.env.UBER_CLIENT_ID,
+      client_secret: process.env.UBER_CLIENT_SECRET,
+    },
+  },
+]
 
 module.exports = apis.reduce((exp, api) => {
-  const lib = require(`node-${api}`)
+  const lib = require(`node-${api.name}`)
   const auth = {
     redirect_uri: 'http://localhost:3000/oauth',
-    ...require(`./private/${api}-auth`),
+    ...api.auth,
   }
   return {
-    [api]: new lib(auth),
+    [api.name]: new lib(auth),
     ...exp,
   }
 }, {})
