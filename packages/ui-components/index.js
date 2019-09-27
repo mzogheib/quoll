e7a0d3ba005c79d615662078788195b8be8fc0e2
Marwan Zogheib <mzogheib@gmail.com>
2019-09-27 11:38:21 +1000

    Update ui-component's Button

--- packages/ui-components/index.js
+++ packages/ui-components/index.js
@@ -1,3 +1,7 @@
+import { ThemeProvider } from 'styled-components'
+
 import button from './src/Button'
+
+export const QuollUIThemeProvider = ThemeProvider
 
 export const Button = button
--- packages/ui-components/src/Button/stories.js
+++ packages/ui-components/src/Button/stories.js
@@ -1,14 +1,58 @@
 import React from 'react'
+import styled from 'styled-components'
+import themes from '@quoll/ui-themes'
+
+import { QuollUIThemeProvider } from '../../index'
 import Button from '.'
 
 export default { title: 'Button' }
 
-export const withText = () => <Button>Hello Button</Button>
+const Row = styled.div`
+  display: flex;
+`
 
-export const withEmoji = () => (
-  <Button>
-    <span role="img" aria-label="so cool">
-      😀 😎 👍 💯
-    </span>
-  </Button>
+const Column = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`
+
+const ButtonWrapper = styled.div`
+  padding: 20px;
+`
+
+const handleClick = buttonVariation =>
+  alert(`Clicked on ${buttonVariation} Button!`)
+
+const buttonVariations = [null, 'Primary', 'Plain']
+const buttonDisabledProp = [false, true]
+
+const renderButton = ({ variation, disabled }) => {
+  const ButtonComponent = variation ? Button[variation] : Button
+  const variationLabel = variation || 'Default'
+  return (
+    <ButtonWrapper key={`${variationLabel}-${disabled}`}>
+      <ButtonComponent
+        disabled={disabled}
+        onClick={() => handleClick(variationLabel)}
+      >
+        {variationLabel} Button
+      </ButtonComponent>
+    </ButtonWrapper>
+  )
+}
+
+export const withText = () => (
+  <QuollUIThemeProvider theme={themes.default}>
+    <Row>
+      {buttonDisabledProp.map(disabled => (
+        <Column key={disabled}>
+          <div>{disabled ? 'Disabled' : 'Enabled'}</div>
+          {buttonVariations.map(variation =>
+            renderButton({ variation, disabled })
+          )}
+        </Column>
+      ))}
+    </Row>
+  </QuollUIThemeProvider>
 )
