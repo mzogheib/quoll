94749b68df840080c341738d2037c0a3b8c31848
Marwan Zogheib <mzogheib@users.noreply.github.com>
2022-07-23 10:00:41 +1000

    Replace custom webpack build with react-scripts

--- packages/client/server.js
+++ packages/client/server.js
@@ -2,13 +2,13 @@ const express = require('express')
 const path = require('path')
 
 const app = express()
-const PORT = process.env.PORT || 3000
+const PORT = process.env.REACT_APP_PORT || 3000
 
-app.use(express.static(path.join(__dirname, 'dist')))
+app.use(express.static(path.join(__dirname, 'build')))
 
 // https://facebook.github.io/create-react-app/docs/deployment#serving-apps-with-client-side-routing
 app.get('*', (req, res) =>
-  res.sendFile(path.join(__dirname, 'dist', 'index.html'))
+  res.sendFile(path.join(__dirname, 'build', 'index.html'))
 )
 
 // eslint-disable-next-line no-console
--- packages/client/src/services/api.js
+++ packages/client/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const baseUrl = process.env.API_URL
+const baseUrl = process.env.REACT_APP_API_URL
 
 let authHeader
 
