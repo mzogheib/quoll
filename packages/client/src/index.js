cb6d45fe4d04596f402f393b9f1bc413c30f1d22
Marwan Zogheib <mzogheib@users.noreply.github.com>
2022-08-14 21:24:39 +1000

    Migrate client to CRA

--- packages/client/src/index.js
+++ packages/client/src/index.js
@@ -1,12 +1,17 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
+import ReactDOM from 'react-dom/client'
 
 import AppRoot from './AppRoot'
+import reportWebVitals from './reportWebVitals'
 
-const render = () => {
-  ReactDOM.render(<AppRoot />, document.getElementById('root'))
-}
+const root = ReactDOM.createRoot(document.getElementById('root'))
+root.render(
+  <React.StrictMode>
+    <AppRoot />
+  </React.StrictMode>
+)
 
-render()
-
-if (module.hot) module.hot.accept('./AppRoot', () => render())
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals()
---
+++ packages/client/src/ui-theme.d.ts
@@ -0,0 +1,2 @@
+declare module '@quoll/ui-themes'
+declare module '@quoll/ui-components'
