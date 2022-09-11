8c26debcd3e5619256a04952f63f01b6bdc0d2f1
Marwan Zogheib <mzogheib@users.noreply.github.com>
2022-09-12 08:05:37 +1000

    Convert index file to tsx

--- packages/client/src/index.tsx
+++ packages/client/src/index.tsx
@@ -1,15 +1,19 @@
 import React from 'react'
-import ReactDOM from 'react-dom/client'
+import { createRoot } from 'react-dom/client'
 
 import AppRoot from './AppRoot'
 import reportWebVitals from './reportWebVitals'
 
-const root = ReactDOM.createRoot(document.getElementById('root'))
-root.render(
-  <React.StrictMode>
-    <AppRoot />
-  </React.StrictMode>
-)
+const container = document.getElementById('root')
+
+if (container) {
+  const root = createRoot(container)
+  root.render(
+    <React.StrictMode>
+      <AppRoot />
+    </React.StrictMode>
+  )
+}
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
