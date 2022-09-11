2482e629a888b5cc6352c2e337743afff4e93171
Marwan Zogheib <mzogheib@users.noreply.github.com>
2022-09-12 08:05:38 +1000

    Convert storage to ts

---
+++ packages/client/src/services/storage.ts
@@ -0,0 +1,22 @@
+const prefix = 'quoll'
+
+const makeKey = (key: string) => `${prefix}-${key}`
+
+const get = (key: string) => {
+  const data = localStorage.getItem(makeKey(key))
+
+  if (data) return JSON.parse(data)
+}
+
+const set = (key: string, data: string) =>
+  localStorage.setItem(makeKey(key), JSON.stringify(data))
+
+const remove = (key: string) => localStorage.removeItem(makeKey(key))
+
+const storageService = {
+  get,
+  set,
+  delete: remove,
+}
+
+export default storageService
