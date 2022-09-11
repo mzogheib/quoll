a7e4346944bc8d6717597bd5761852d6ee016d05
Marwan Zogheib <mzogheib@users.noreply.github.com>
2022-09-12 08:05:38 +1000

    Convert user to ts

--- packages/client/src/services/api.ts
+++ packages/client/src/services/api.ts
@@ -1,7 +1,9 @@
 import axios, { AxiosRequestConfig } from 'axios'
 
-const baseURL = process.env.REACT_APP_API_URL
+const makeUrl = (endpoint: string) =>
+  `${process.env.REACT_APP_API_URL}/${endpoint}`
 
+// TODO: come up with a better way than this. Redux or a cookie.
 let authHeader: AxiosRequestConfig['headers']
 
 const authenticate = (userId: string) => {
@@ -10,49 +12,50 @@ const authenticate = (userId: string) => {
 
 type RequestParams = {
   endpoint: string
-  params: AxiosRequestConfig['params']
-  payload: AxiosRequestConfig['data']
+  params?: AxiosRequestConfig['params']
+  payload?: AxiosRequestConfig['data']
 }
 
-const get = ({ endpoint, params }: RequestParams) =>
-  new Promise((resolve, reject) => {
-    axios({
-      method: 'GET',
-      baseURL,
-      url: endpoint,
-      params,
-      headers: authHeader,
-    })
-      .then((response) => resolve(response.data))
-      .catch(reject)
-  })
+const get = async <ResponseDataType>({ endpoint, params }: RequestParams) => {
+  const config = { headers: authHeader }
 
-const post = ({ endpoint, params, payload }: RequestParams) =>
-  new Promise((resolve, reject) => {
-    axios({
-      method: 'POST',
-      baseURL,
-      url: endpoint,
-      params,
-      data: payload,
-      headers: authHeader,
-    })
-      .then((response) => resolve(response.data))
-      .catch(reject)
-  })
+  const search = new URLSearchParams(params)
+  const searchString = search.toString()
 
-const deleteReq = ({ endpoint, params }: RequestParams) =>
-  new Promise((resolve, reject) => {
-    axios({
-      method: 'DELETE',
-      baseURL,
-      url: endpoint,
-      params,
-      headers: authHeader,
-    })
-      .then((response) => resolve(response.data))
-      .catch(reject)
-  })
+  const url = `${makeUrl(endpoint)}${searchString}`
+
+  const response = await axios.get<ResponseDataType>(url, config)
+
+  return response.data
+}
+
+const post = async <ResponseDataType>({ endpoint, payload }: RequestParams) => {
+  const config = { headers: authHeader }
+
+  const response = await axios.post<ResponseDataType>(
+    makeUrl(endpoint),
+    payload,
+    config
+  )
+
+  return response.data
+}
+
+const deleteReq = async <ResponseDataType>({
+  endpoint,
+  params,
+}: RequestParams) => {
+  const config = { headers: authHeader }
+
+  const search = new URLSearchParams(params)
+  const searchString = search.toString()
+
+  const url = `${makeUrl(endpoint)}${searchString}`
+
+  const response = await axios.delete<ResponseDataType>(url, config)
+
+  return response.data
+}
 
 const apiService = {
   authenticate,
