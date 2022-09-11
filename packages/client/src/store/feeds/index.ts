68d64914663a4b8499bb7fe45833d1d4baceab4b
Marwan Zogheib <mzogheib@users.noreply.github.com>
2022-09-12 08:05:39 +1000

    Conver feed store to ts

--- packages/client/src/store/feeds/index.ts
+++ packages/client/src/store/feeds/index.ts
@@ -1,46 +1,58 @@
+import { Action, Dispatch } from 'redux'
+
 import feedServices from '../../services/feeds'
 
-export const setFeedConnected = (name, value) => ({
-  type: 'SET_FEED_CONNECTED',
+enum FeedActionType {
+  SetConnected = 'SET_FEED_CONNECTED',
+  SetAuthenticating = 'SET_FEED_AUTHENTICATING',
+}
+
+interface FeedAction extends Action<FeedActionType> {
+  name: string
+  value: boolean
+}
+
+export const setFeedConnected = (name: string, value: boolean) => ({
+  type: FeedActionType.SetConnected,
   name,
   value,
 })
 
-export const setFeedAuthenticating = (name, value) => ({
-  type: 'SET_FEED_AUTHENTICATING',
+export const setFeedAuthenticating = (name: string, value: boolean) => ({
+  type: FeedActionType.SetAuthenticating,
   name,
   value,
 })
 
-export const getOauthUrl = (name) => {
+export const getOauthUrl = (name: string) => {
   const feedService = feedServices.find((feed) => feed.name === name)
-  return (dispatch) => {
+  return (dispatch: Dispatch) => {
     dispatch(setFeedAuthenticating(name, true))
     return feedService
-      .getOauthUrl()
+      ?.getOauthUrl()
       .then((url) => url)
       .finally(() => dispatch(setFeedAuthenticating(name, false)))
   }
 }
 
-export const authenticateFeed = (name, code) => {
+export const authenticateFeed = (name: string, code: string) => {
   const feedService = feedServices.find((feed) => feed.name === name)
-  return (dispatch) => {
+  return (dispatch: Dispatch) => {
     dispatch(setFeedAuthenticating(name, true))
     return feedService
-      .authenticate({ code })
+      ?.authenticate({ code })
       .then(() => dispatch(setFeedConnected(name, true)))
       .finally(() => dispatch(setFeedAuthenticating(name, false)))
   }
 }
 
-export const disconnectFeed = (name) => {
+export const disconnectFeed = (name: string) => {
   const feedService = feedServices.find((feed) => feed.name === name)
-  return (dispatch) => {
+  return (dispatch: Dispatch) => {
     dispatch(setFeedAuthenticating(name, true))
     return (
       feedService
-        .disconnect()
+        ?.disconnect()
         // BE may return a message for further, manual instructions
         .then((message) => {
           dispatch(setFeedConnected(name, false))
@@ -57,15 +69,15 @@ const defaultFeeds = feedServices.map((config) => ({
   isAuthenticating: false,
 }))
 
-const feeds = (state = defaultFeeds, action) => {
+const feeds = (state = defaultFeeds, action: FeedAction) => {
   switch (action.type) {
-    case 'SET_FEED_CONNECTED':
+    case FeedActionType.SetConnected:
       return state.map((feed) =>
         feed.name === action.name
           ? { ...feed, isConnected: action.value }
           : feed
       )
-    case 'SET_FEED_AUTHENTICATING':
+    case FeedActionType.SetAuthenticating:
       return state.map((feed) =>
         feed.name === action.name
           ? { ...feed, isAuthenticating: action.value }
