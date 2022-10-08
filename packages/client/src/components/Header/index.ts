4d8c6131e30d13c76e624f0d3bb90fbdc104cacf
Marwan Zogheib <mzogheib@users.noreply.github.com>
2022-10-08 22:02:07 +1100

    Migrate Header to ts

---
+++ packages/client/src/components/Header/index.ts
@@ -0,0 +1,16 @@
+import styled, { css } from 'styled-components'
+
+const Header = styled.div(
+  ({ theme: { colors } }) => css`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    min-height: 40px;
+    background-color: ${colors.matterhorn};
+    color: ${colors.mediumAquamarine};
+    font-size: 20px;
+    font-weight: 500;
+  `
+)
+
+export default Header
