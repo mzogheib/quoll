f0caab27595765b074df4ca8c2a9ade7093d2f00
Marwan Zogheib <mzogheib@users.noreply.github.com>
2023-05-14 18:16:51 +1000

    docs: add Chromatic Storybook support (#134)
    * Add chromatic
    
    * Add chromatic script placeholder
    
    * Add dotenv
    
    * ignroe env file
    
    * Add another .env file
    
    * Create a chromatic script

---
+++ packages/ui-components/scripts/chromatic.js
@@ -0,0 +1,7 @@
+require('dotenv').config()
+const { spawn } = require('child_process')
+
+const { CHROMATIC_PROJECT_TOKEN } = process.env
+const opts = { stdio: 'inherit', shell: true }
+
+spawn(`npx chromatic --project-token=${CHROMATIC_PROJECT_TOKEN}`, opts)
