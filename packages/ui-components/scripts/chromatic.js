fdff7e44bc5a5fa16af29ce518a7ec0d01568ff0
Marwan Zogheib <mzogheib@users.noreply.github.com>
2023-06-07 13:45:37 +1000

    docs: auto accept chromatic changes

--- packages/ui-components/scripts/chromatic.js
+++ packages/ui-components/scripts/chromatic.js
@@ -4,4 +4,7 @@ const { spawn } = require('child_process')
 const { CHROMATIC_PROJECT_TOKEN } = process.env
 const opts = { stdio: 'inherit', shell: true }
 
-spawn(`npx chromatic --project-token=${CHROMATIC_PROJECT_TOKEN}`, opts)
+spawn(
+  `npx chromatic --project-token=${CHROMATIC_PROJECT_TOKEN} --auto-accept-changes`,
+  opts
+)
