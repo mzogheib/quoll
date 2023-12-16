ff534acd1e250fc082cee9a79bab1182b721a21e
Marwan Zogheib <mzogheib@users.noreply.github.com>
2023-12-17 09:03:06 +1100

    fix(client-web): create map props only when the google map api is ready

--- packages/client-web/src/components/Map/index.tsx
+++ packages/client-web/src/components/Map/index.tsx
@@ -1,11 +1,17 @@
 import { Wrapper, Status } from "@googlemaps/react-wrapper";
 
 import MapPlaceholder from "./Placeholder";
-import MapComponent, { Props } from "./Component";
+import MapComponent, { Props as ComponentProps } from "./Component";
 
 const apiKey = process.env.REACT_APP_GOOGLE_MAPS_KEY;
 
-const Map = (props: Props) => {
+type OwnProps = {
+  onMapLoaded: () => void;
+};
+
+type Props = ComponentProps & OwnProps;
+
+const Map = ({ onMapLoaded, ...rest }: Props) => {
   if (!apiKey) {
     return <MapPlaceholder text="Hmm could not load map - missing API key" />;
   }
@@ -15,14 +21,27 @@ const Map = (props: Props) => {
       case Status.LOADING:
         return <MapPlaceholder text="Loading" />;
       case Status.SUCCESS:
-        return <MapComponent {...props} />;
+        return <MapComponent {...rest} />;
       case Status.FAILURE:
       default:
         return <MapPlaceholder text="Hmm could not load map" />;
     }
   };
 
-  return <Wrapper apiKey={apiKey} libraries={["geometry"]} render={render} />;
+  const handleCallback = (status: Status) => {
+    if (status === Status.SUCCESS) {
+      onMapLoaded();
+    }
+  };
+
+  return (
+    <Wrapper
+      apiKey={apiKey}
+      libraries={["geometry"]}
+      render={render}
+      callback={handleCallback}
+    />
+  );
 };
 
 export default Map;
