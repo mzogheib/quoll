643ff9d409ef48218953b8662ef6f1ccf5d16bdc
Marwan Zogheib <mzogheib@users.noreply.github.com>
2023-12-17 21:30:35 +1100

    Use env variable for api url

--- packages/client-web/src/__mocks__/handlers.ts
+++ packages/client-web/src/__mocks__/handlers.ts
@@ -3,14 +3,17 @@ import { http, HttpResponse } from "msw";
 import userData from "./data/user.json";
 import timelineData from "./data/timeline.json";
 
+const makeApiUrl = (endpoint: string) =>
+  `${process.env.REACT_APP_API_URL}${endpoint}`;
+
 export const handlers = [
-  http.post("http://localhost:3001/api/signup", () => {
+  http.post(makeApiUrl("/signup"), () => {
     return HttpResponse.json(userData);
   }),
-  http.post("http://localhost:3001/api/login", () => {
+  http.post(makeApiUrl("/login"), () => {
     return HttpResponse.json(userData);
   }),
-  http.get("http://localhost:3001/api/timeline", () => {
+  http.get(makeApiUrl("/timeline"), () => {
     return HttpResponse.json(timelineData);
   }),
 ];
