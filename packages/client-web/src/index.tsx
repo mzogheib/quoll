b3d2d5718e4b12b962e119e97ce8624da20844a9
Marwan Zogheib <mzogheib@users.noreply.github.com>
2023-12-17 21:38:36 +1100

    Remove .then

--- packages/client-web/src/index.tsx
+++ packages/client-web/src/index.tsx
@@ -23,7 +23,7 @@ const initApp = async () => {
   renderRoot();
 };
 
-initApp().then(() => {});
+initApp();
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
