0a0119ad13d9391c852c022a1dbe6bdb12058fcb
Marwan Zogheib <mzogheib@users.noreply.github.com>
2023-12-17 21:21:13 +1100

    Add msw handlers and mock data

---
+++ packages/client-web/src/__mocks__/index.ts
@@ -0,0 +1,19 @@
+import { http, HttpResponse } from "msw";
+import { setupWorker } from "msw/browser";
+
+import userData from "./data/user.json";
+import timelineData from "./data/timeline.json";
+
+const handlers = [
+  http.post("http://localhost:3001/api/signup", () => {
+    return HttpResponse.json(userData);
+  }),
+  http.post("http://localhost:3001/api/login", () => {
+    return HttpResponse.json(userData);
+  }),
+  http.get("http://localhost:3001/api/timeline", () => {
+    return HttpResponse.json(timelineData);
+  }),
+];
+
+export const worker = setupWorker(...handlers);
