a8ecf19bfd0df420a04fcc019a3832e2f42108cf
Marwan Zogheib <mzogheib@users.noreply.github.com>
2023-12-25 09:33:53 +1100

    refactor(ui-components): add breakpoints and colorPalette from ui-primitives

--- packages/ui-components/src/themes.ts
+++ packages/ui-components/src/themes.ts
@@ -1,26 +1,5 @@
+import { colorPalette, breakpoints } from "@quoll/ui-primitives";
 import { css } from "styled-components";
-
-// Color names from https://www.color-blindness.com/color-name-hue/
-const colorPalette = {
-  black: "#000000",
-  gainsboro: "#DCDCDC",
-  grey: "#7F7F7F",
-  matterhorn: "#4F4F4F",
-  mediumAquamarine: "#6FCF97",
-  mineShaft: "#333333",
-  royalBlue: "#2F80ED",
-  transparent: "transparent",
-  white: "#FFFFFF",
-  whiteSmoke: "#F2F2F2",
-};
-
-const breakpoints = {
-  xs: 0,
-  sm: 544,
-  md: 768,
-  lg: 992,
-  xl: 1281,
-};
 
 const breakpointUp = (size: number) => (styles: TemplateStringsArray) => css`
   @media (min-width: ${size}px) {
