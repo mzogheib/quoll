e466a07dafa7e3116789ebe3f7fcffb4b2e493e0
Marwan Zogheib <mzogheib@users.noreply.github.com>
2023-12-24 18:33:42 +1100

    feat(ui-primitives): create a new ui-primitives package

---
+++ packages/ui-primitives/src/index.ts
@@ -0,0 +1,30 @@
+/**
+ * A palette of colours with human readable names.
+ *
+ * @remarks
+ *
+ * Color names from https://www.color-blindness.com/color-name-hue
+ */
+export const colorPalette = {
+  black: "#000000",
+  gainsboro: "#DCDCDC",
+  grey: "#7F7F7F",
+  matterhorn: "#4F4F4F",
+  mediumAquamarine: "#6FCF97",
+  mineShaft: "#333333",
+  royalBlue: "#2F80ED",
+  transparent: "transparent",
+  white: "#FFFFFF",
+  whiteSmoke: "#F2F2F2",
+};
+
+/**
+ * The breakpoints for various screen sizes.
+ */
+export const breakpoints = {
+  xs: 0,
+  sm: 544,
+  md: 768,
+  lg: 992,
+  xl: 1281,
+};
