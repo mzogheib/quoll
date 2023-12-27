86e0b7d82ab600e234f69dd906a06f410aac6b53
Marwan Zogheib <mzogheib@users.noreply.github.com>
2023-12-28 09:18:24 +1100

    Add DateBar and DatePicker

---
+++ packages/client-mobile/src/date-bar/ui/DateBar/styles.ts
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+  actions: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+    width: "100%",
+  },
+});
+
+export default styles;
