324c57ced052c137462738009c5210096f5559bf
Marwan Zogheib <mzogheib@users.noreply.github.com>
2023-12-29 08:44:37 +1100

    Delete useIsNarrow

--- packages/client-mobile/src/dimensions/index.ts
+++ packages/client-mobile/src/dimensions/index.ts
@@ -107,22 +107,3 @@ export const useResponsiveStyles = <Name extends string>(
 
   return stylesMap_[screenWidth];
 };
-
-/**
- * Returns whether the screen is narrow or not.
- *
- * @remarks
- *
- * Narrow is defined by screen breakpoint values from the design system.
- *
- * @returns
- *
- * `true` if the screen is narrow
- *
- * `false` if the screen is wide
- */
-export const useIsNarrow = () => {
-  const { width } = useWindowDimensions();
-
-  return width <= breakpoints.md;
-};
