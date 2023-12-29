e0731306a788cb1485a67144457580ccc34ab702
Marwan Zogheib <mzogheib@users.noreply.github.com>
2023-12-30 10:37:59 +1100

    Request location permission on android

--- packages/client-mobile/src/geolocation/index.ts
+++ packages/client-mobile/src/geolocation/index.ts
@@ -3,20 +3,60 @@ import Geolocation, {
   GeolocationError,
 } from "@react-native-community/geolocation";
 import { Coords } from "./types";
+import { PermissionsAndroid } from "react-native";
+
+const locationNotPermittedError = {
+  ACTIVITY_NULL: 4,
+  PERMISSION_DENIED: 1,
+  POSITION_UNAVAILABLE: 2,
+  TIMEOUT: 3,
+  code: 1,
+  message: "Location permission was not granted.",
+};
+
+const requestPermission = async () => {
+  try {
+    const granted = await PermissionsAndroid.request(
+      PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
+      {
+        title: "Location access",
+        message: "Allow access to your location for the best experience.",
+        buttonNeutral: "Ask Me Later",
+        buttonNegative: "Cancel",
+        buttonPositive: "Allow",
+      },
+    );
+
+    return granted === PermissionsAndroid.RESULTS.GRANTED;
+  } catch {
+    return false;
+  }
+};
 
 export const useGeolocation = (initialCoords: Coords) => {
   const [coords, setCoords] = useState(initialCoords);
   const [error, setError] = useState<GeolocationError | undefined>(undefined);
 
   useEffect(() => {
-    Geolocation.getCurrentPosition(
-      (info) => {
-        setCoords(info.coords);
-      },
-      (error_) => {
-        setError(error_);
-      },
-    );
+    const getPosition = async () => {
+      const permissionGranted = await requestPermission();
+
+      if (!permissionGranted) {
+        setError(locationNotPermittedError);
+        return;
+      }
+
+      Geolocation.getCurrentPosition(
+        (info) => {
+          setCoords(info.coords);
+        },
+        (error_) => {
+          setError(error_);
+        },
+      );
+    };
+
+    getPosition();
   }, []);
 
   return {
