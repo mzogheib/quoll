5eafab67cb512e6fd0f68e23e22cfdd0788c5217
Marwan Zogheib <mzogheib@users.noreply.github.com>
2023-12-29 11:27:41 +1100

    Set region to user location

--- packages/client-mobile/src/Map/index.tsx
+++ packages/client-mobile/src/Map/index.tsx
@@ -1,14 +1,24 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import MapView from "react-native-maps";
+import Geolocation from "@react-native-community/geolocation";
+
 import styles from "./styles";
 
 // Melbourne, Victoria
-const initialCoords = {
+const defaultCoords = {
   latitude: -37.8136,
   longitude: 144.9631,
 };
 
 export const Map = () => {
+  const [initialCoords, setInitialCoords] = useState(defaultCoords);
+
+  useEffect(() => {
+    Geolocation.getCurrentPosition((info) => {
+      setInitialCoords(info.coords);
+    });
+  }, []);
+
   const region = {
     latitude: initialCoords.latitude,
     longitude: initialCoords.longitude,
@@ -16,5 +26,5 @@ export const Map = () => {
     longitudeDelta: 0.1,
   };
 
-  return <MapView style={styles.wrapper} initialRegion={region} />;
+  return <MapView style={styles.wrapper} region={region} showsUserLocation />;
 };
