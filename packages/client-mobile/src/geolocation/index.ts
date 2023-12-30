8d0af9adf750e2b87d4355b7c21c1a6934e36393
Marwan Zogheib <mzogheib@users.noreply.github.com>
2023-12-30 14:45:21 +1100

    feat(client-mobile): show user location for Android (#163)

--- packages/client-mobile/src/geolocation/index.ts
+++ packages/client-mobile/src/geolocation/index.ts
@@ -1,22 +1,67 @@
 import { useEffect, useState } from "react";
-import Geolocation, {
-  GeolocationError,
-} from "@react-native-community/geolocation";
+import Geolocation from "@react-native-community/geolocation";
 import { Coords } from "./types";
+import { Platform, PermissionsAndroid } from "react-native";
+
+type GeolocationError =
+  | "PERMISSION_DENIED"
+  | "POSITION_UNAVAILABLE"
+  | "TIMEOUT"
+  | "ACTIVITY_NULL";
+
+const errors: Record<number, GeolocationError> = {
+  1: "PERMISSION_DENIED",
+  2: "POSITION_UNAVAILABLE",
+  3: "TIMEOUT",
+  4: "ACTIVITY_NULL",
+};
+
+const requestPermission = async () => {
+  if (Platform.OS !== "android") return true;
+
+  try {
+    const granted = await PermissionsAndroid.request(
+      PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
+      {
+        title: "Location access",
+        message: "Allow access to your location for the best experience.",
+        buttonNeutral: "Ask Me Later",
+        buttonNegative: "Cancel",
+        buttonPositive: "Allow",
+      },
+    );
+
+    return granted === PermissionsAndroid.RESULTS.GRANTED;
+  } catch {
+    return false;
+  }
+};
 
 export const useGeolocation = (initialCoords: Coords) => {
   const [coords, setCoords] = useState(initialCoords);
   const [error, setError] = useState<GeolocationError | undefined>(undefined);
 
   useEffect(() => {
-    Geolocation.getCurrentPosition(
-      (info) => {
-        setCoords(info.coords);
-      },
-      (error_) => {
-        setError(error_);
-      },
-    );
+    const getPosition = async () => {
+      const permissionGranted = await requestPermission();
+
+      if (!permissionGranted) {
+        setError("PERMISSION_DENIED");
+        return;
+      }
+
+      Geolocation.getCurrentPosition(
+        (info) => {
+          setCoords(info.coords);
+        },
+        ({ code }) => {
+          const err = errors[code] ?? "PERMISSION_DENIED";
+          setError(err);
+        },
+      );
+    };
+
+    getPosition();
   }, []);
 
   return {
