e5e194a5257118589fc09b61e7af82c9e3bce724
Marwan Zogheib <mzogheib@users.noreply.github.com>
2024-01-02 21:24:52 +1100

    Simplify babel config

--- packages/client-mobile/babel.config.js
+++ packages/client-mobile/babel.config.js
@@ -4,8 +4,6 @@ module.exports = {
     [
       "module-resolver",
       {
-        root: ["./src"],
-        extensions: [".ios.js", ".android.js", ".js", ".ts", ".tsx", ".json"],
         alias: {
           "@components": "./src/components",
           "@modules": "./src/modules",
