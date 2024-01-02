6235d39a0e61f88cc5822187bb075837969d58a0
Marwan Zogheib <mzogheib@users.noreply.github.com>
2024-01-03 10:55:10 +1100

    Watch the root folder

--- packages/client-mobile/metro.config.js
+++ packages/client-mobile/metro.config.js
@@ -1,3 +1,4 @@
+const path = require("path");
 const { getDefaultConfig, mergeConfig } = require("@react-native/metro-config");
 
 /**
@@ -6,6 +7,8 @@ const { getDefaultConfig, mergeConfig } = require("@react-native/metro-config");
  *
  * @type {import('metro-config').MetroConfig}
  */
-const config = {};
+const config = {
+  watchFolders: [path.resolve(__dirname, "../..")],
+};
 
 module.exports = mergeConfig(getDefaultConfig(__dirname), config);
