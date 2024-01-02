a2df741e6fa8b7d6233da417e560ad6096fc8606
Marwan Zogheib <mzogheib@users.noreply.github.com>
2024-01-02 21:46:09 +1100

    fix(client-mobile): add babel config for path aliases (#168)

--- packages/client-mobile/babel.config.js
+++ packages/client-mobile/babel.config.js
@@ -1,3 +1,16 @@
 module.exports = {
   presets: ["module:@react-native/babel-preset"],
+  plugins: [
+    [
+      "module-resolver",
+      {
+        alias: {
+          "@components": "./src/components",
+          "@modules": "./src/modules",
+          "@screens": "./src/screens",
+          "@ui-primitives": "./src/ui-primitives",
+        },
+      },
+    ],
+  ],
 };
