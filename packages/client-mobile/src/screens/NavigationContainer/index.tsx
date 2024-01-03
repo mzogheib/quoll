de48adda983df1fe157469b03e5f52ff6abf6069
Marwan Zogheib <mzogheib@users.noreply.github.com>
2024-01-03 14:38:59 +1100

    Determine the current route name

--- packages/client-mobile/src/screens/NavigationContainer/index.tsx
+++ packages/client-mobile/src/screens/NavigationContainer/index.tsx
@@ -1,12 +1,28 @@
 import React, { ReactNode } from "react";
-import { NavigationContainer as RNNavigationContainer } from "@react-navigation/native";
+import {
+  NavigationState,
+  NavigationContainer as RNNavigationContainer,
+} from "@react-navigation/native";
 
 type Props = {
   children: ReactNode;
 };
 
 const NavigationContainer = ({ children }: Props) => {
-  return <RNNavigationContainer>{children}</RNNavigationContainer>;
+  const handleStateChange = (state: NavigationState | undefined) => {
+    if (!state) return;
+
+    const { index, routeNames } = state;
+    const currentRouteName = routeNames[index];
+
+    console.log("currentRouteName", currentRouteName);
+  };
+
+  return (
+    <RNNavigationContainer onStateChange={handleStateChange}>
+      {children}
+    </RNNavigationContainer>
+  );
 };
 
 export default NavigationContainer;
