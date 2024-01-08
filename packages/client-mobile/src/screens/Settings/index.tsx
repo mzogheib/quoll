5b67d51916d176f322874c6006cab7b446e58d6b
Marwan Zogheib <mzogheib@users.noreply.github.com>
2024-01-09 07:55:30 +1100

    feat(client-mobile): add settings screen (#186)

--- packages/client-mobile/src/screens/Settings/index.tsx
+++ packages/client-mobile/src/screens/Settings/index.tsx
@@ -1,13 +1,23 @@
 import React from "react";
 import { Text, View } from "react-native";
 
+import styles from "./styles";
+
 import { ScreenProps } from "../types";
 import ScreenTemplate from "../ScreenTemplate";
+import FeedSettings from "@components/FeedSettings";
 
 const SettingsScreen = (_: ScreenProps<"settings">) => {
   return (
     <ScreenTemplate>
-      <Text>Settings Screen</Text>
+      <View style={styles.wrapper}>
+        <View style={styles.content}>
+          <Text style={styles.title}>Feeds</Text>
+          <View style={styles.feedSettingsWrapper}>
+            <FeedSettings title="Photos" onConnect={() => {}} />
+          </View>
+        </View>
+      </View>
     </ScreenTemplate>
   );
 };
