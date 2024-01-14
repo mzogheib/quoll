8bf321071b057df1070cedce05e204de59c44e47
Marwan Zogheib <mzogheib@users.noreply.github.com>
2024-01-14 20:53:17 +1100

    Add alert to allow access to photos

--- packages/client-mobile/src/modules/media/logic/index.ts
+++ packages/client-mobile/src/modules/media/logic/index.ts
@@ -1,25 +1,37 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { CameraRoll } from "@react-native-camera-roll/camera-roll";
+import { Alert, Linking } from "react-native";
+
+const checkIsPermitted = async () => {
+  try {
+    const result = await CameraRoll.getPhotos({ first: 1 });
+    return !!result;
+  } catch {
+    return false;
+  }
+};
 
 export const useMedia = () => {
   const [isConnected, setIsConnected] = useState(false);
 
-  useEffect(() => {
-    const checkIsConnected = async () => {
-      try {
-        const result = await CameraRoll.getPhotos({ first: 1 });
+  const connect = async () => {
+    const isPermitted = await checkIsPermitted();
+    setIsConnected(isPermitted);
 
-        setIsConnected(!!result);
-      } catch {
-        // ...
-      }
-    };
-
-    checkIsConnected();
-  }, []);
-
-  const connect = () => {
-    setIsConnected(true);
+    if (!isPermitted) {
+      Alert.alert(
+        "Allow access",
+        "Please allow access to your photos and videos.",
+        [
+          {
+            text: "Go to settings",
+            isPreferred: true,
+            onPress: () => Linking.openSettings(),
+          },
+          { text: "Cancel" },
+        ],
+      );
+    }
   };
 
   const disconnect = () => {
