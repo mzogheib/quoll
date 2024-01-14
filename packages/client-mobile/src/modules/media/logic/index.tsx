0667d5b0e78b93050b8ac023af79a415509003be
Marwan Zogheib <mzogheib@users.noreply.github.com>
2024-01-14 12:42:15 +1100

    Create useMedia hook

--- packages/client-mobile/src/modules/media/logic/index.tsx
+++ packages/client-mobile/src/modules/media/logic/index.tsx
@@ -1,6 +1,14 @@
-/**
- * Seek access to device media.
- */
-export const connectToMedia = () => {
-  console.log("connected");
+import { useState } from "react";
+
+export const useMedia = () => {
+  const [isPermitted, setIsPermitted] = useState(false);
+
+  const requestPermission = () => {
+    setIsPermitted(true);
+  };
+
+  return {
+    requestPermission,
+    isPermitted,
+  };
 };
--- packages/client-mobile/src/screens/Settings/index.tsx
+++ packages/client-mobile/src/screens/Settings/index.tsx
@@ -6,16 +6,20 @@ import styles from "./styles";
 import { ScreenProps } from "../types";
 import ScreenTemplate from "../ScreenTemplate";
 import FeedSettings from "@components/FeedSettings";
-import { connectToMedia } from "@modules/media/logic";
+import { useMedia } from "@modules/media/logic";
 
 const SettingsScreen = (_: ScreenProps<"settings">) => {
+  const media = useMedia();
+
+  console.log(media.isPermitted);
+
   return (
     <ScreenTemplate>
       <View style={styles.wrapper}>
         <View style={styles.content}>
           <Text style={styles.title}>Feeds</Text>
           <View style={styles.feedSettingsWrapper}>
-            <FeedSettings title="Photos" onConnect={connectToMedia} />
+            <FeedSettings title="Photos" onConnect={media.requestPermission} />
           </View>
         </View>
       </View>
