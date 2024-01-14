b2f3310b1d2ff6a801b19e8efa48097170b0b662
Marwan Zogheib <mzogheib@users.noreply.github.com>
2024-01-14 21:39:05 +1100

    Add comment

--- packages/client-mobile/src/modules/alert/logic/index.ts
+++ packages/client-mobile/src/modules/alert/logic/index.ts
@@ -1,5 +1,11 @@
 import { Alert, Linking } from "react-native";
 
+/**
+ * Displays an alert to prompt the user to allow access to a resource via the
+ * device settings. The primary button will open the app's settings page.
+ *
+ * @param message the message to display in the alert.
+ */
 export const promptAllowAccess = (message: string) => {
   Alert.alert("Allow access", message, [
     {
