fb8ff5c42ec44d7c1eb29be922b86022183e25be
Marwan Zogheib <mzogheib@users.noreply.github.com>
2024-01-14 20:26:14 +1100

    Add permission check

--- packages/client-mobile/src/modules/media/logic/index.ts
+++ packages/client-mobile/src/modules/media/logic/index.ts
@@ -1,7 +1,22 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { CameraRoll } from "@react-native-camera-roll/camera-roll";
 
 export const useMedia = () => {
   const [isPermitted, setIsPermitted] = useState(false);
+
+  useEffect(() => {
+    const checkIsPermitted = async () => {
+      try {
+        const result = await CameraRoll.getPhotos({ first: 1 });
+
+        setIsPermitted(!!result);
+      } catch {
+        // ...
+      }
+    };
+
+    checkIsPermitted();
+  }, []);
 
   const requestPermission = () => {
     setIsPermitted(true);
