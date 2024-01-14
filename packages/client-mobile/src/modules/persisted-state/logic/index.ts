582529fe5e46558b14f8275b8e1d4c648c9d14c2
Marwan Zogheib <mzogheib@users.noreply.github.com>
2024-01-15 08:15:47 +1100

    Add comment

--- packages/client-mobile/src/modules/persisted-state/logic/index.ts
+++ packages/client-mobile/src/modules/persisted-state/logic/index.ts
@@ -2,11 +2,19 @@ import { MMKVLoader, useMMKVStorage } from "react-native-mmkv-storage";
 
 const MMKV = new MMKVLoader().initialize();
 
+/**
+ * Returns a stateful value and a function to update it. Perists the value
+ * between app launches.
+ *
+ * @param key a unique key for the persisted value
+ * @param initialValue the initial value
+ * @returns a tuple of the value and update function
+ */
 export const usePersistedState = <V>(
   key: string,
-  defaultValue: V,
+  initialValue: V,
 ): [V, (newValue: V) => void] => {
-  const [value, setValue] = useMMKVStorage(key, MMKV, defaultValue);
+  const [value, setValue] = useMMKVStorage(key, MMKV, initialValue);
 
   return [value, setValue];
 };
