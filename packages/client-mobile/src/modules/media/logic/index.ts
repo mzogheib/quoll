3fb0488e11263c69786db47431aeabcefce7ab85
Marwan Zogheib <mzogheib@users.noreply.github.com>
2024-01-14 21:31:40 +1100

    Add android permission check

--- packages/client-mobile/src/modules/media/logic/index.ts
+++ packages/client-mobile/src/modules/media/logic/index.ts
@@ -1,11 +1,37 @@
 import { useCallback, useEffect, useState } from "react";
+import { Platform, PermissionsAndroid } from "react-native";
 import { CameraRoll } from "@react-native-camera-roll/camera-roll";
 import { promptAllowAccess } from "@modules/alert/logic";
 
+const platformVersion = Number(Platform.Version);
+
+const checkIsPermittedAndroid = async () => {
+  if (platformVersion >= 33) {
+    return Promise.all([
+      PermissionsAndroid.check(
+        PermissionsAndroid.PERMISSIONS.READ_MEDIA_IMAGES,
+      ),
+      PermissionsAndroid.check(PermissionsAndroid.PERMISSIONS.READ_MEDIA_VIDEO),
+    ]).then(
+      ([hasReadMediaImagesPermission, hasReadMediaVideoPermission]) =>
+        hasReadMediaImagesPermission && hasReadMediaVideoPermission,
+    );
+  } else {
+    return PermissionsAndroid.check(
+      PermissionsAndroid.PERMISSIONS.READ_EXTERNAL_STORAGE,
+    );
+  }
+};
+
 const checkIsPermitted = async () => {
   try {
-    const result = await CameraRoll.getPhotos({ first: 1 });
-    return !!result;
+    if (Platform.OS === "ios") {
+      const result = await CameraRoll.getPhotos({ first: 1 });
+      return !!result;
+    } else {
+      const result = await checkIsPermittedAndroid();
+      return !!result;
+    }
   } catch {
     return false;
   }
