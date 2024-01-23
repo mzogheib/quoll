66a26b528924f1fb16080a69910c26c88ead5f7c
Marwan Zogheib <mzogheib@users.noreply.github.com>
2024-01-24 08:17:05 +1100

    Rename file

--- packages/client-web/src/store/index.ts
+++ packages/client-web/src/store/index.ts
@@ -5,7 +5,7 @@ import { composeWithDevTools } from "redux-devtools-extension";
 import user from "./user";
 import date from "./date";
 import feeds from "./feeds";
-import timeline from "../modules/timeline/model";
+import timeline from "../modules/timeline/model/store";
 
 const reducer = combineReducers({
   user,
