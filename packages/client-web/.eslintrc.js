814b200c3ba162cf01a4658450703b3f2fd3be7f
Marwan Zogheib <mzogheib@users.noreply.github.com>
2024-01-26 10:21:49 +1100

    Add eslint rule to prevent absolute imports

--- packages/client-web/.eslintrc.js
+++ packages/client-web/.eslintrc.js
@@ -1,3 +1,16 @@
 module.exports = {
   extends: ["react-app", "react-app/jest"],
+  rules: {
+    "no-restricted-imports": [
+      "error",
+      {
+        patterns: [
+          {
+            group: ["*/*/modules/*/*"],
+            message: "Import from @modules/ instead.",
+          },
+        ],
+      },
+    ],
+  },
 };
