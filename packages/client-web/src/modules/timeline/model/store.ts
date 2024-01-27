22a40f0bb03642e985ffa4f79931f59ca192d565
Marwan Zogheib <mzogheib@users.noreply.github.com>
2024-01-27 14:18:12 +1100

    Replace manual timeline store with factory made

--- packages/client-web/src/modules/timeline/model/store.ts
+++ packages/client-web/src/modules/timeline/model/store.ts
@@ -1,59 +1,21 @@
-import { Action } from "redux";
-
-import { RootState } from "../../../store";
-
 import { Entry } from "../types";
-
-enum TimelineActionType {
-  SetFetching = "SET_TIMELINE_FETCHING",
-  SetEntries = "SET_TIMELINE_ENTRIES",
-}
-
-interface SetTimelineFetchingAction
-  extends Action<TimelineActionType.SetFetching> {
-  value: boolean;
-}
-
-export const setTimelineFetching = (
-  value: boolean,
-): SetTimelineFetchingAction => ({
-  type: TimelineActionType.SetFetching,
-  value,
-});
-
-interface SetTimelineEntriesAction
-  extends Action<TimelineActionType.SetEntries> {
-  entries: Entry[];
-}
-
-export const setEntries = (entries: Entry[]): SetTimelineEntriesAction => ({
-  type: TimelineActionType.SetEntries,
-  entries,
-});
-
-export const selectTimeline = (state: RootState) => state.timeline;
-
-type TimelineAction = SetTimelineFetchingAction | SetTimelineEntriesAction;
+import { makeStore } from "store/factory";
 
 export type TimelineState = {
   isFetching: boolean;
   entries: Entry[];
 };
 
-const defaultState: TimelineState = { isFetching: true, entries: [] };
-
-const timeline = (
-  state: TimelineState = defaultState,
-  action: TimelineAction,
-): TimelineState => {
-  switch (action.type) {
-    case TimelineActionType.SetFetching:
-      return { ...state, isFetching: action.value };
-    case TimelineActionType.SetEntries:
-      return { entries: action.entries, isFetching: false };
-    default:
-      return state;
-  }
+const defaultState: TimelineState = {
+  isFetching: true,
+  entries: [],
 };
 
-export default timeline;
+const { reducer, useStore } = makeStore<TimelineState>(
+  "timeline",
+  defaultState,
+);
+
+export default reducer;
+
+export const useTimelineStore = useStore;
