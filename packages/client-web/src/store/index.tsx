0f607c9c4ff1a2c093a6ce33ace6ec8bba8ebf41
Marwan Zogheib <mzogheib@users.noreply.github.com>
2024-01-27 15:32:14 +1100

    Rewrite imports

--- packages/client-web/src/store/index.tsx
+++ packages/client-web/src/store/index.tsx
@@ -3,31 +3,25 @@ import { legacy_createStore, combineReducers } from "redux";
 import { Provider } from "react-redux";
 import { composeWithDevTools } from "redux-devtools-extension";
 
-import { reducer as user, State as UserState } from "modules/user/model/store";
-import { reducer as date, State as DateState } from "modules/date/model/store";
-import {
-  reducer as feeds,
-  State as FeedsState,
-} from "modules/feeds/model/store";
-import {
-  reducer as timeline,
-  State as TimelineState,
-} from "modules/timeline/model/store";
+import * as userStore from "modules/user/model/store";
+import * as dateStore from "modules/date/model/store";
+import * as feedsStore from "modules/feeds/model/store";
+import * as timelineStore from "modules/timeline/model/store";
 
 const reducer = combineReducers({
-  user,
-  date,
-  feeds,
-  timeline,
+  user: userStore.useStore,
+  date: dateStore.useStore,
+  feeds: feedsStore.useStore,
+  timeline: timelineStore.useStore,
 });
 
 const store = legacy_createStore(reducer, composeWithDevTools());
 
 export type RootState = {
-  user: UserState;
-  date: DateState;
-  feeds: FeedsState;
-  timeline: TimelineState;
+  user: userStore.State;
+  date: dateStore.State;
+  feeds: feedsStore.State;
+  timeline: timelineStore.State;
 };
 
 export const StoreProvider = ({ children }: { children: ReactNode }) => (
