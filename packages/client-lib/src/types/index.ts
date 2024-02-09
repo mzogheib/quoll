eb812858206bbf041bc62e08a2fc2df3f013e155
Marwan Zogheib <mzogheib@users.noreply.github.com>
2024-02-09 14:29:04 +1100

    docs(client-lib): add TSDoc for ISO8601Date type

--- packages/client-lib/src/types/index.ts
+++ packages/client-lib/src/types/index.ts
@@ -1,1 +1,8 @@
+/**
+ * A date string formatted as `YYYY-MM-DD`.
+ *
+ * @remarks the purpose of this type is to add meaning to date variables that
+ * need to take this format. It could be enhanced to enforce the string
+ * structure but that becomes overly complicated
+ */
 export type ISO8601Date = string;
