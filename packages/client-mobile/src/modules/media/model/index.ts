88b3306e8a7d36479a0f70a5a7b79db7341dd7b0
Marwan Zogheib <mzogheib@users.noreply.github.com>
2024-02-09 13:47:07 +1100

    refactor(client-mobile): use zustand store in media model

--- packages/client-mobile/src/modules/media/model/index.ts
+++ packages/client-mobile/src/modules/media/model/index.ts
@@ -1,22 +1,43 @@
-import { useCallback, useEffect, useState } from "react";
+import { useCallback, useEffect } from "react";
 
 import { promptAllowAccess } from "@modules/alert/logic";
-import { usePersistedState } from "@modules/persisted-state/logic";
 import { MediaItem } from "../types";
 import { checkIsPermitted, getMedia, requestPermission } from "../service";
+import { makeStore } from "../../../store";
+import { usePersistedState } from "@modules/persisted-state/logic";
+
+type MediaState = {
+  isConnecting: boolean;
+  isCheckingPermission: boolean;
+  isRefreshing: boolean;
+  value: MediaItem[];
+};
+
+const defaultState: MediaState = {
+  isConnecting: false,
+  isCheckingPermission: true,
+  isRefreshing: false,
+  value: [],
+};
+
+const useMediaStore = makeStore(defaultState);
 
 export const useMediaModel = () => {
-  const [isConnected, setIsConnected] = usePersistedState("isConnected", false);
-  const [isConnecting, setIsConnecting] = useState(false);
-  const [isCheckingPermission, setIsCheckingPermission] = useState(true);
-  const [isRefreshing, setIsRefreshing] = useState(false);
-  const [value, setValue] = usePersistedState<MediaItem[]>("mediaValue", []);
+  // isConnected should be in device state so that it can be persisted
+  // between app launches.
+  const [isConnected, setIsConnected] = usePersistedState(
+    "media::isConnected",
+    false,
+  );
+  const { state, setProperty } = useMediaStore();
+
+  const { isConnecting, isCheckingPermission, isRefreshing, value } = state;
 
   const syncPermission = useCallback(async () => {
-    setIsCheckingPermission(true);
+    setProperty("isCheckingPermission", true);
     const isPermitted = await checkIsPermitted();
     setIsConnected(isPermitted);
-    setIsCheckingPermission(false);
+    setProperty("isCheckingPermission", false);
 
     return isPermitted;
   }, []);
@@ -30,7 +51,7 @@ export const useMediaModel = () => {
   }, [syncPermission]);
 
   const connect = async () => {
-    setIsConnecting(true);
+    setProperty("isConnecting", true);
     const isPermitted = await checkIsPermitted();
 
     if (isPermitted) {
@@ -44,23 +65,23 @@ export const useMediaModel = () => {
         promptAllowAccess("Quoll works best with your photos and videos.");
       }
     }
-    setIsConnecting(false);
+    setProperty("isConnecting", false);
   };
 
   const disconnect = () => {
-    setValue([]);
+    setProperty("value", []);
     setIsConnected(false);
   };
 
   const refresh = useCallback(
     async (params: { createdAfter: Date; createdBefore: Date }) => {
-      setIsRefreshing(true);
+      setProperty("isRefreshing", true);
       const response = await getMedia(params);
       const newValue = response.edges.map(
         (photoIdentifier) => photoIdentifier.node,
       );
-      setValue(newValue);
-      setIsRefreshing(false);
+      setProperty("value", newValue);
+      setProperty("isRefreshing", false);
     },
     [],
   );
