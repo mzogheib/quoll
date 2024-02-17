8e8fcc92c194f029073f7e80b54cd708cbfb7094
Marwan Zogheib <mzogheib@users.noreply.github.com>
2024-02-17 15:33:25 +1100

    refactor(client-mobile): move HomeScreen code to controller

---
+++ packages/client-mobile/src/screens/Home/controller.ts
@@ -0,0 +1,39 @@
+import { makeISO8601Date } from "@quoll/lib";
+
+import { MarkerProps } from "@modules/map/ui/Map";
+import { useDateViewModel } from "@modules/date-bar/view-model";
+import { useTimelineViewModel } from "@modules/timeline/view-model";
+
+const useController = () => {
+  const { date, setDate } = useDateViewModel();
+  const { entries, fetchTimeline } = useTimelineViewModel(date);
+
+  const handleDateChange = (newDate: Date) => {
+    const formattedDate = makeISO8601Date(newDate);
+    setDate(formattedDate);
+    fetchTimeline(formattedDate);
+  };
+
+  const markers: MarkerProps[] = entries
+    .filter(({ locationStart, polyline, mediaUri }) => {
+      const hasLocation = locationStart?.latitude && locationStart.longitude;
+
+      return hasLocation && !polyline && !!mediaUri;
+    })
+    .map(({ locationStart, mediaUri }) => ({
+      // The ! assertions are safe based on above filter
+      coordinate: {
+        latitude: locationStart!.latitude,
+        longitude: locationStart!.longitude,
+      },
+      image: { uri: mediaUri! },
+    }));
+
+  return {
+    markers,
+    date,
+    handleDateChange,
+  };
+};
+
+export default useController;
--- packages/client-mobile/src/screens/Home/index.tsx
+++ packages/client-mobile/src/screens/Home/index.tsx
@@ -1,41 +1,18 @@
 import React from "react";
 import { View } from "react-native";
-import { makeISO8601Date } from "@quoll/lib";
 
 import { useStyles } from "./styles";
 
-import { Map, MarkerProps } from "@modules/map/ui/Map";
+import { Map } from "@modules/map/ui/Map";
 import { DateBar } from "@modules/date-bar/ui/DateBar";
-import { useDateViewModel } from "@modules/date-bar/view-model";
-import { useTimelineViewModel } from "@modules/timeline/view-model";
 import { ScreenProps } from "../config";
 import ScreenTemplate from "../ScreenTemplate";
+import useController from "./controller";
 
 const HomeScreen = ({ route }: ScreenProps<"home">) => {
   const styles = useStyles();
-  const { date, setDate } = useDateViewModel();
-  const { entries, fetchTimeline } = useTimelineViewModel(date);
 
-  const handleDateChange = (newDate: Date) => {
-    const formattedDate = makeISO8601Date(newDate);
-    setDate(formattedDate);
-    fetchTimeline(formattedDate);
-  };
-
-  const markers: MarkerProps[] = entries
-    .filter(({ locationStart, polyline, mediaUri }) => {
-      const hasLocation = locationStart?.latitude && locationStart.longitude;
-
-      return hasLocation && !polyline && !!mediaUri;
-    })
-    .map(({ locationStart, mediaUri }) => ({
-      // The ! assertions are safe based on above filter
-      coordinate: {
-        latitude: locationStart!.latitude,
-        longitude: locationStart!.longitude,
-      },
-      image: { uri: mediaUri! },
-    }));
+  const { markers, date, handleDateChange } = useController();
 
   return (
     <ScreenTemplate screenName={route.name}>
