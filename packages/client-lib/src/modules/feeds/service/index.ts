d5489d3ae9a2cc2f35c51ee6799aa7853094a731
Marwan Zogheib <mzogheib@users.noreply.github.com>
2024-02-24 16:19:16 +1100

    Move feedsservice to client-lib

--- packages/client-lib/src/modules/feeds/service/index.ts
+++ packages/client-lib/src/modules/feeds/service/index.ts
@@ -1,7 +1,40 @@
 import { FeedName } from "@quoll/lib";
+import { AuthenticatedApiService } from "../../../utils";
 
-export type FeedsService = {
-  getOauthUrl: (feedName: FeedName) => Promise<string>;
-  authenticate: (feed: FeedName, payload: { code: string }) => Promise<null>;
-  deauthorize: (feed: FeedName) => Promise<string | undefined>;
+type AuthenticatePayload = {
+  code: string;
 };
+
+const endpoint = "/feed-auth";
+
+export class FeedsService extends AuthenticatedApiService {
+  async getOauthUrl(feed: FeedName) {
+    return this.request<string>({
+      method: "GET",
+      endpoint,
+      params: { feed },
+    });
+  }
+
+  async authenticate(feed: FeedName, payload: AuthenticatePayload) {
+    return this.request<null>({
+      method: "POST",
+      endpoint,
+      payload,
+      params: { feed },
+    });
+  }
+
+  async deauthorize(feed: FeedName) {
+    return this.request<null>({
+      method: "DELETE",
+      endpoint,
+      params: { feed },
+    }).then(() => {
+      // TODO: this should come from the BE
+      if (feed === "moves") {
+        return "Remember to revoke access in the Moves app.";
+      }
+    });
+  }
+}
--- packages/client-web/src/modules/feeds/service/index.ts
+++ packages/client-web/src/modules/feeds/service/index.ts
@@ -1,47 +1,8 @@
-import { AuthenticatedApiService, FeedsService } from "@quoll/client-lib";
-import { FeedName } from "@quoll/lib";
+import { FeedsService } from "@quoll/client-lib";
 
 import { getAccessToken } from "services/session";
 
-type AuthenticatePayload = {
-  code: string;
-};
-
-const endpoint = "/feed-auth";
-
-class _FeedsService extends AuthenticatedApiService implements FeedsService {
-  async getOauthUrl(feed: FeedName) {
-    return this.request<string>({
-      method: "GET",
-      endpoint,
-      params: { feed },
-    });
-  }
-
-  async authenticate(feed: FeedName, payload: AuthenticatePayload) {
-    return this.request<null>({
-      method: "POST",
-      endpoint,
-      payload,
-      params: { feed },
-    });
-  }
-
-  async deauthorize(feed: FeedName) {
-    return this.request<null>({
-      method: "DELETE",
-      endpoint,
-      params: { feed },
-    }).then(() => {
-      // TODO: this should come from the BE
-      if (feed === "moves") {
-        return "Remember to revoke access in the Moves app.";
-      }
-    });
-  }
-}
-
-const feedsService = new _FeedsService(
+const feedsService = new FeedsService(
   getAccessToken,
   `${process.env.REACT_APP_API_URL}`,
 );
