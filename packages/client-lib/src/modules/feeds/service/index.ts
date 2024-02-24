8327ef242a3c2022664fc3f1941948998324dcc8
Marwan Zogheib <mzogheib@users.noreply.github.com>
2024-02-24 15:21:15 +1100

    refactor: update feedsservice type

--- packages/client-lib/src/modules/feeds/service/index.ts
+++ packages/client-lib/src/modules/feeds/service/index.ts
@@ -3,5 +3,5 @@ import { FeedName } from "@quoll/lib";
 export type FeedsService = {
   getOauthUrl: (feedName: FeedName) => Promise<string>;
   authenticate: (feed: FeedName, payload: { code: string }) => Promise<null>;
-  deauthorize: (feed: FeedName) => Promise<null>;
+  deauthorize: (feed: FeedName) => Promise<string | undefined>;
 };
