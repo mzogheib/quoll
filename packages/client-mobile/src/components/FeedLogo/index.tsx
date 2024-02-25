706366eccd3691e6bb26f9093c89fc50655d091b
Marwan Zogheib <mzogheib@users.noreply.github.com>
2024-02-25 19:24:07 +1100

    Add FeedLogo component

---
+++ packages/client-mobile/src/components/FeedLogo/index.tsx
@@ -0,0 +1,32 @@
+import Icon from "react-native-vector-icons/MaterialIcons";
+import { FeedName } from "@quoll/lib";
+import { colorPalette } from "@quoll/ui-primitives";
+
+const iconMap = {
+  toshl: null,
+  strava: "directions-run",
+  uber: null,
+  moves: null,
+  media: "image",
+  location: "my-location",
+};
+
+interface Props {
+  name: FeedName | "location";
+  size: number;
+  isGrayscale?: boolean;
+}
+
+const FeedLogo = ({ name, size, isGrayscale }: Props) => {
+  const iconName = iconMap[name];
+
+  if (!iconName) return null;
+
+  const color = isGrayscale
+    ? colorPalette.matterhorn
+    : colorPalette.mediumAquamarine;
+
+  return <Icon name={iconName} size={size} color={color} aria-label={name} />;
+};
+
+export default FeedLogo;
