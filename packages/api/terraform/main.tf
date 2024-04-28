9191a7264045108e646181dd7c8e0a32774844cf
Marwan Zogheib <mzogheib@users.noreply.github.com>
2024-04-28 11:21:21 +1000

    build: add foundational Terraform file and workflow (#217)

---
+++ packages/api/terraform/main.tf
@@ -0,0 +1,12 @@
+terraform {
+  required_providers {
+    random = {
+      source  = "hashicorp/random"
+      version = "3.4.3"
+    }
+  }
+
+  required_version = ">= 1.1.0"
+}
+
+resource "random_pet" "my-pet" {}
