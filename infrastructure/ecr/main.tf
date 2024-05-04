1eacb7093408601a728f24697272d7c3dbf25ef3
Marwan Zogheib <mzogheib@users.noreply.github.com>
2024-05-04 17:47:53 +1000

    make repo name a secret

--- infrastructure/ecr/main.tf
+++ infrastructure/ecr/main.tf
@@ -11,16 +11,17 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_ecr_repository" "quoll" {
-  name                 = "quoll"
+
+variable "repo_name" {
+  description = "The name of the private ECR repo"
+  type        = string
+}
+
+resource "aws_ecr_repository" "main" {
+  name                 = var.repo_name
   image_tag_mutability = "IMMUTABLE"
 
   image_scanning_configuration {
     scan_on_push = true
   }
-}
-
-output "ecr_repository_url" {
-  description = "The URL of the ECR repository"
-  value       = aws_ecr_repository.quoll.repository_url
 }
