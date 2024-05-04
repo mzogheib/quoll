ab8e0a6f5fe56a549cc9760bfa7ce00225191fd1
Marwan Zogheib <mzogheib@users.noreply.github.com>
2024-05-05 09:41:36 +1000

    build: simplify shell script and revert action

--- scripts/extract-tag.sh
+++ scripts/extract-tag.sh
@@ -3,18 +3,13 @@
 # A script to extract the semver version from a git tag.
 # For example, @quoll/some-package@1.2.3 --> 1.2.3
 
-# Function to check if the input string matches the required format
-validate_input() {
-    if [[ ! "$1" =~ ^@[^@]+/[^@]+@[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
-        echo "Error: Input string must be in the format '@<namespace>/<name>@<semver-version>'"
-        exit 1
-    fi
-}
+tag=$1
 
-# Validate the input format
-validate_input "$1"
+if [[ ! "$tag" =~ ^@[^@]+/[^@]+@[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
+    echo "Error: Input string must be in the format '@<namespace>/<name>@<semver-version>'"
+    exit 1
+fi
 
-# Extract the version using sed
-version=$(echo "$1" | sed 's/.*@[^@]*@\([^@]*\)/\1/')
+version=$(echo "$tag" | sed 's/.*@[^@]*@\([^@]*\)/\1/')
 
 echo "$version"
