07f0b5cb4f851629f36c513a54a91f33dd70bd67
Marwan Zogheib <mzogheib@users.noreply.github.com>
2024-05-13 08:37:28 +1000

    build: fix resource name

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -37,5 +37,5 @@ resource "aws_instance" "api_server" {
 
 output "instance_public_ip" {
   description = "Public IP address of the EC2 instance"
-  value       = aws_instance.app_server.public_ip
+  value       = aws_instance.api_server.public_ip
 }
