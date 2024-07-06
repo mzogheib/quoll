9a6a158c45b831f34c66c2f1fb376819d4f798c8
Marwan Zogheib <mzogheib@users.noreply.github.com>
2024-07-06 18:30:06 +1000

    Update dotenv and express to es imports

--- packages/api/src/server.ts
+++ packages/api/src/server.ts
@@ -1,10 +1,12 @@
-require("dotenv").config();
-const express = require("express");
+import dotenv from "dotenv";
+import express from "express";
+
 const mongoose = require("mongoose");
 const cors = require("cors");
-const app = express();
 const bodyParser = require("body-parser");
 const routes = require("./routes");
+
+dotenv.config();
 
 mongoose.connect(process.env.MONGODB_CONNECTION_STRING, {
   useNewUrlParser: true,
@@ -15,21 +17,18 @@ const db = mongoose.connection;
 if (!db) console.log("Error connecting db");
 else console.log("Db connected successfully");
 
+const app = express();
+
 app.set("port", process.env.PORT || 3001);
 
-app.use(
-  bodyParser.urlencoded({
-    extended: false,
-  }),
-);
+app.use(bodyParser.urlencoded({ extended: false }));
+
 app.use(bodyParser.json());
 
 app.use(cors({ origin: process.env.CORS_ALLOWED_ORIGIN }));
 
 app.use("/api", routes);
 
-// Listen for requests on this port
-const server = app.listen(app.get("port"), () => {
-  const port = server.address().port;
-  console.log("Listening on port " + port);
-});
+const port = app.get("port");
+
+app.listen(port, () => console.log("Listening on port " + port));
