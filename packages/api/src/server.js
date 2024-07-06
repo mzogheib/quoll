785037cbcbddd7b70d597617ce7dd73414cea3f6
Marwan Zogheib <mzogheib@users.noreply.github.com>
2024-07-06 16:41:00 +1000

    refactor(api): remove old port in use bug workaround

--- packages/api/src/server.js
+++ packages/api/src/server.js
@@ -33,35 +33,3 @@ const server = app.listen(app.get("port"), () => {
   const port = server.address().port;
   console.log("Listening on port " + port);
 });
-
-// TODO understand wtf is going on with nodemon and express.
-// Seems like the express server stays alive when nodemon restarts the app, which leads to port in use error "Error: listen EADDRINUSE :::3001"
-// Looks related https://github.com/remy/nodemon/issues/1247
-
-// Below makes it better sometimes. Inspired by http://glynnbird.tumblr.com/post/54739664725/graceful-server-shutdown-with-nodejs-and-express
-// This function is called when you want the server to die gracefully
-// i.e. wait for existing connections
-const gracefulShutdown = () => {
-  console.log("Received kill signal, shutting down gracefully.");
-  server.close(() => {
-    console.log("Closed out connections.");
-    process.exit(0);
-  });
-
-  // if after
-  setTimeout(() => {
-    console.error(
-      "Could not close connections in time, forcefully shutting down.",
-    );
-    process.exit(0);
-  }, 10 * 1000);
-};
-
-// listen for TERM signal .e.g. kill
-process.on("SIGTERM", gracefulShutdown);
-
-// listen for INT signal e.g. Ctrl-C
-process.on("SIGINT", gracefulShutdown);
-
-// e.g. used by nodemon, type rs while nodemon is running
-process.once("SIGUSR2", gracefulShutdown);
