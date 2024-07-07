51fbdc10ffcdf093fffe74a587fe14d058e84da3
Marwan Zogheib <mzogheib@users.noreply.github.com>
2024-07-08 08:42:31 +1000

    refactor(api): update toshl typings

--- packages/api/typings/node-toshl/index.d.ts
+++ packages/api/typings/node-toshl/index.d.ts
@@ -4,6 +4,37 @@ declare module "node-toshl" {
       redirect_uri: string;
       client_id: string;
       client_secret: string;
-    }) {}
+    });
+
+    oauth: {
+      url(): string;
+      token(params: { code: string }): Promise<{
+        expires_in: number;
+        access_token: string;
+        refresh_token: string;
+      }>;
+      refresh(params: { refresh_token: string }): Promise<{
+        expires_in: number;
+        access_token: string;
+        refresh_token: string;
+      }>;
+      deauthorize(params: {
+        access_token: string;
+      }): Promise<{ access_token: string }>;
+    };
+
+    tags: {
+      list(params: {
+        access_token: string;
+      }): Promise<{ id: string; name: string }[]>;
+    };
+
+    entries: {
+      list(params: {
+        from: string;
+        to: string;
+        access_token: string;
+      }): Promise<{ id: string }[]>;
+    };
   }
 }
