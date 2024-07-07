591e8365717d9f597499f6f81db9f1b4ce0ff4d9
Marwan Zogheib <mzogheib@users.noreply.github.com>
2024-07-07 19:03:25 +1000

    refactor(api): update user controller to ts

---
+++ packages/api/src/controllers/users.controller.ts
@@ -0,0 +1,64 @@
+import { FeedName, User } from "@quoll/lib";
+import { IAuth, UserDoc, UserModel } from "../models/user.model";
+
+const DefaultFeeds = [
+  { name: "strava", auth: null },
+  { name: "toshl", auth: null },
+];
+
+function sanitizeUser(user: UserDoc): User {
+  const feeds = user.feeds.map((feed) => {
+    return {
+      name: feed.name,
+      isConnected: !!feed.auth,
+    };
+  });
+
+  return {
+    _id: user._id.toString(),
+    feeds,
+  };
+}
+
+export const createUser = async () => {
+  const user = await UserModel.create({ feeds: DefaultFeeds });
+  return sanitizeUser(user);
+};
+
+export const login = async (userId: string) => {
+  const user = await get(userId);
+  return sanitizeUser(user);
+};
+
+export const get = async (userId: string): Promise<UserDoc> => {
+  const user = await UserModel.findById(userId);
+
+  if (user === null) throw new Error("User not found");
+
+  return user;
+};
+
+export const setFeedAuth = async (
+  userId: string,
+  feedName: FeedName,
+  data: IAuth,
+) => {
+  const user = await get(userId);
+
+  user.feeds.forEach((feed) => {
+    if (feed.name !== feedName) return;
+
+    feed.auth = data;
+  });
+
+  await UserModel.updateOne({ _id: user._id }, user);
+};
+
+export const getFeedAuth = async (userId: string, feedName: FeedName) => {
+  const user = await get(userId);
+  const feed = user.feeds.find((feed) => feed.name === feedName);
+
+  if (feed === undefined) throw new Error("Feed not found");
+
+  return feed.auth;
+};
--- packages/api/src/models/user.model.ts
+++ packages/api/src/models/user.model.ts
@@ -1,10 +1,33 @@
-import { Schema, model } from "mongoose";
+import { FeedName } from "@quoll/lib";
+import { HydratedDocument, Schema, model } from "mongoose";
 
-const schema = new Schema({
+export type IAuth = {
+  expiry_time: String;
+  access_token: String;
+  refresh_token: String;
+};
+
+type IFeed = {
+  name: FeedName;
+  auth: IAuth | null;
+};
+
+type IUser = {
+  feeds: IFeed[];
+};
+
+export type UserDoc = HydratedDocument<IUser>;
+
+const schema = new Schema<IUser>({
   feeds: {
-    type: Array,
+    type: [
+      {
+        name: { type: String, required: true },
+        auth: { type: Object },
+      },
+    ],
     required: true,
   },
 });
 
-export const UserModel = model("User", schema, "users");
+export const UserModel = model<IUser>("User", schema, "users");
