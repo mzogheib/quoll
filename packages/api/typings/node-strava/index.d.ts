276c8ecc6e5d44510cb99a20f24a0826951fd2bc
Marwan Zogheib <mzogheib@users.noreply.github.com>
2024-07-07 21:11:10 +1000

    refactor(api): update strava typings

--- packages/api/typings/node-strava/index.d.ts
+++ packages/api/typings/node-strava/index.d.ts
@@ -4,6 +4,41 @@ declare module "node-strava" {
       redirect_uri: string;
       client_id: string;
       client_secret: string;
-    }) {}
+    });
+
+    oauth: {
+      url(): string;
+      token(params: { code: string }): Promise<{
+        expires_in: number;
+        access_token: string;
+        refresh_token: string;
+      }>;
+      refresh(params: { refresh_token: string }): Promise<{
+        expires_in: number;
+        access_token: string;
+        refresh_token: string;
+      }>;
+      deauthorize(params: {
+        access_token: string;
+      }): Promise<{ access_token: string }>;
+    };
+
+    athlete: {
+      activities: {
+        list(params: {
+          after: number;
+          before: number;
+          per_page: number;
+          access_token: string;
+        }): Promise<{ id: number }[]>;
+      };
+    };
+
+    activities: {
+      get(params: {
+        id: number;
+        access_token: string;
+      }): Promise<{ id: number }[]>;
+    };
   }
 }
