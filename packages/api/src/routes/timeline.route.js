78364c4787b9195135cabe2c09f801382916a6bb
Marwan Zogheib <mzogheib@users.noreply.github.com>
2024-07-08 11:12:50 +1000

    refactor(api): update timeline route to async await

--- packages/api/src/routes/timeline.route.js
+++ packages/api/src/routes/timeline.route.js
@@ -1,17 +1,21 @@
 import { get as getUser } from "../controllers/users.controller";
 import { get as getTimeline } from "../controllers/timeline.controller";
 
-export const get = (req, res) => {
-  const { from, to } = req.query;
-  const { userId } = req;
-
+export const get = async (req, res) => {
   const respond = ({ status, message }) => res.status(status).json(message);
   const onSuccess = (data) => respond({ status: 200, message: data });
   const onError = (error) =>
     respond({ status: error.status || 500, message: error.message });
 
-  getUser(userId)
-    .then((user) => getTimeline(from, to, user))
-    .then(onSuccess)
-    .catch(onError);
+  try {
+    const { from, to } = req.query;
+    const { userId } = req;
+
+    const user = await getUser(userId);
+    const timeline = await getTimeline(from, to, user);
+
+    onSuccess(timeline);
+  } catch (error) {
+    onError(error);
+  }
 };
