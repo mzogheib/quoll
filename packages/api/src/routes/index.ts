369e74abd317b6f2471d1296e6f4c8ae9e56e7f7
Marwan Zogheib <mzogheib@users.noreply.github.com>
2024-07-08 16:34:01 +1000

    refactor(api): fix typing in route index

--- packages/api/src/routes/index.ts
+++ packages/api/src/routes/index.ts
@@ -2,12 +2,13 @@ import { Router } from "express";
 
 import { login, signup, authenticate } from "./users.route";
 import {
-  checkAuth,
+  checkAuth as checkFeedAuth,
   getOAuthUrl,
-  authenticate as _authenticate,
+  authenticate as authenticateFeed,
   deauthorize,
 } from "./feed-auth.route";
 import { get } from "./timeline.route";
+import { AuthenticatedRequest } from "./types";
 
 const router = Router();
 
@@ -18,11 +19,20 @@ router.route("/signup").post(signup);
 router
   .route("/feed-auth")
   .all(authenticate)
-  .all(checkAuth)
-  .get(getOAuthUrl)
-  .post(_authenticate)
+  // TODO can this be removed? Do any of these endpoints need an authenticated feed?
+  .all((req, res, next) =>
+    checkFeedAuth(req as AuthenticatedRequest, res, next),
+  )
+  .get((req, res) => getOAuthUrl(req as AuthenticatedRequest, res))
+  .post((req, res) => authenticateFeed(req as AuthenticatedRequest, res))
   .delete(deauthorize);
 
-router.route("/timeline").all(authenticate).all(checkAuth).get(get);
+router
+  .route("/timeline")
+  .all(authenticate)
+  .all((req, res, next) =>
+    checkFeedAuth(req as AuthenticatedRequest, res, next),
+  )
+  .get((req, res) => get(req as AuthenticatedRequest, res));
 
 export default router;
