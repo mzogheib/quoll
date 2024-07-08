9749b95c12ec5d14a845ce0bfb80154d3ca68982
Marwan Zogheib <mzogheib@users.noreply.github.com>
2024-07-08 11:08:53 +1000

    refactor(api): update timeline controller to ts

---
+++ packages/api/src/controllers/timeline.controller.ts
@@ -0,0 +1,27 @@
+import * as feedServices from "../feed-services";
+import * as feedAdapters from "../feed-adapters";
+import { UserDoc } from "../models/user.model";
+
+export const get = async (from: string, to: string, user: UserDoc) => {
+  const authenticatedFeeds = user.feeds.filter((feed) => feed.auth !== null);
+
+  const promises = authenticatedFeeds.map(async (feed) => {
+    // TODO: decouple 'media' from the api
+    if (feed.name === "media") return [];
+
+    const feedService = feedServices[feed.name];
+    const feedAdapter = feedAdapters[feed.name].adapter;
+
+    const accessToken = feed.auth!.access_token;
+
+    const data = await feedService.getData(from, to, accessToken);
+
+    // TODO add the types
+    return feedAdapter(data as any);
+  });
+
+  const arraysOfEntries = await Promise.all(promises);
+  const entries = arraysOfEntries.reduce((prev, next) => prev.concat(next), []);
+
+  return entries.sort((a, b) => a.timeStart - b.timeStart);
+};
