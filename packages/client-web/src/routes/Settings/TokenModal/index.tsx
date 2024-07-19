06f0c663b6c4c43201771f08cc1204176da1f607
Marwan Zogheib <mzogheib@users.noreply.github.com>
2024-07-20 09:30:32 +1000

    refactor(client-web): update TokenModal to use new modal components

--- packages/client-web/src/routes/Settings/TokenModal/index.tsx
+++ packages/client-web/src/routes/Settings/TokenModal/index.tsx
@@ -1,23 +1,6 @@
-import styled from "styled-components";
-import { Button, IconButton, Modal } from "@quoll/ui-components";
-import { Input } from "@quoll/ui-components";
 import { useState } from "react";
-
-const Header = styled.div`
-  display: flex;
-  justify-content: flex-end;
-  padding: 10px;
-`;
-
-const InnerWrapper = styled.div`
-  padding: 20px;
-`;
-
-const Actions = styled.div`
-  margin: 30px 0 0;
-  display: flex;
-  justify-content: flex-end;
-`;
+import { ButtonPrimary, Modal } from "@quoll/ui-components";
+import { Input } from "@quoll/ui-components";
 
 interface Props {
   isOpen: boolean;
@@ -36,19 +19,17 @@ const TokenModal = ({ isOpen, onCancel, onSubmit }: Props) => {
 
   return (
     <Modal isOpen={isOpen} onRequestClose={onCancel}>
-      <Header>
-        <IconButton icon="Close" size={30} onClick={onCancel} />
-      </Header>
-      <InnerWrapper>
+      <Modal.Inner>
+        <Modal.Header onClose={onCancel} />
         <Input
           value={value}
           onChange={setValue}
           placeholder="Enter your personal access token"
         />
-        <Actions>
-          <Button onClick={handleSubmit}>Submit</Button>
-        </Actions>
-      </InnerWrapper>
+        <Modal.Actions>
+          <ButtonPrimary onClick={handleSubmit}>Submit</ButtonPrimary>
+        </Modal.Actions>
+      </Modal.Inner>
     </Modal>
   );
 };
