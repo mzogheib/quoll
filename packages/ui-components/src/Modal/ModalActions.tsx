9bcdc2bec965ccfaf49b0e35a80eb020759e0480
Marwan Zogheib <mzogheib@users.noreply.github.com>
2024-07-20 09:18:54 +1000

    feat(ui-components): add an align prop to Modal.Actions

--- packages/ui-components/src/Modal/ModalActions.tsx
+++ packages/ui-components/src/Modal/ModalActions.tsx
@@ -1,8 +1,31 @@
-import styled from "styled-components";
+import React, { HTMLAttributes } from "react";
+import styled, { css } from "styled-components";
 
-export const ModalActions = styled.div`
-  display: flex;
-  justify-content: flex-end;
-  gap: 10px;
-  margin-top: 10px;
-`;
+const alignMap = {
+  start: "flex-start",
+  center: "center",
+  end: "flex-end",
+} as const;
+
+type Align = keyof typeof alignMap;
+
+type DivProps = HTMLAttributes<HTMLDivElement>;
+
+type OwnProps = {
+  align?: Align;
+};
+
+type Props = OwnProps & DivProps;
+
+const _ModalActions = styled.div<{ $align: Align }>(
+  ({ $align }) => css`
+    display: flex;
+    justify-content: ${alignMap[$align]};
+    gap: 10px;
+    margin-top: 10px;
+  `,
+);
+
+export const ModalActions = ({ align = "end", ...rest }: Props) => (
+  <_ModalActions $align={align} {...rest} />
+);
