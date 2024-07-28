b7a1abb3f4e75374afc037107f10de10089a7d17
Marwan Zogheib <mzogheib@users.noreply.github.com>
2024-07-28 11:35:11 +1000

    feat(ui-components): add direction prop to modal actions

--- packages/ui-components/src/Modal/ModalActions.tsx
+++ packages/ui-components/src/Modal/ModalActions.tsx
@@ -9,23 +9,38 @@ const alignMap = {
 
 type Align = keyof typeof alignMap;
 
+const directionMap = {
+  row: "row",
+  "row-reverse": "row-reverse",
+  column: "column",
+  "column-reverse": "column-reverse",
+} as const;
+
+type Direction = keyof typeof directionMap;
+
 type DivProps = HTMLAttributes<HTMLDivElement>;
 
 type OwnProps = {
+  /** Flex alignment for the children */
   align?: Align;
+  /** Flex direction for the children */
+  direction?: Direction;
 };
 
 type Props = OwnProps & DivProps;
 
-const _ModalActions = styled.div<{ $align: Align }>(
-  ({ $align }) => css`
+const _ModalActions = styled.div<{ $align: Align; $direction: Direction }>(
+  ({ $align, $direction }) => css`
     display: flex;
+    flex-direction: ${directionMap[$direction]};
     justify-content: ${alignMap[$align]};
     gap: 10px;
     margin-top: 15px;
   `,
 );
 
-export const ModalActions = ({ align = "end", ...rest }: Props) => (
-  <_ModalActions $align={align} {...rest} />
-);
+export const ModalActions = ({
+  align = "end",
+  direction = "row",
+  ...rest
+}: Props) => <_ModalActions $align={align} $direction={direction} {...rest} />;
