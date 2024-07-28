cc8c758d15d6a63c128273a7970db5a2c3d51962
Marwan Zogheib <mzogheib@users.noreply.github.com>
2024-07-28 10:01:13 +1000

    Reset all models on user logout

--- packages/client-web/src/modules/user/view-model/index.ts
+++ packages/client-web/src/modules/user/view-model/index.ts
@@ -1,11 +1,15 @@
 import { useCallback } from "react";
 
 import { useUserModel } from "../model";
-import { useFeedsModel } from "../../feeds/model";
+import { useFeedsModel } from "modules/feeds/model";
+import { useTimelineModel } from "modules/timeline/model";
+import { useDateModel } from "modules/date/model";
 
 export const useUserViewModel = () => {
   const userModel = useUserModel();
   const feedsModel = useFeedsModel();
+  const timelineModel = useTimelineModel();
+  const dateModel = useDateModel();
 
   const { login } = userModel;
   const { setConnected } = feedsModel;
@@ -21,8 +25,17 @@ export const useUserViewModel = () => {
     [login, setConnected],
   );
 
+  const _logout = useCallback(async () => {
+    timelineModel.reset();
+    feedsModel.reset();
+    dateModel.reset();
+    userModel.reset();
+    await userModel.logout();
+  }, [dateModel, feedsModel, timelineModel, userModel]);
+
   return {
     ...userModel,
     login: _login,
+    logout: _logout,
   };
 };
