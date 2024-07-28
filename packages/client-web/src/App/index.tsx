6f333ef516505ec5976e26883fbbdca2c3ad9d99
Marwan Zogheib <mzogheib@users.noreply.github.com>
2024-07-29 08:00:36 +1000

    refactor(client-web): return jsx directly in App

--- packages/client-web/src/App/index.tsx
+++ packages/client-web/src/App/index.tsx
@@ -71,36 +71,32 @@ const App = () => {
     history.push("/settings");
   };
 
-  const renderApp = () => {
-    return (
-      <Wrapper>
-        <SideBar onHelpClick={handleSideBarHelpClick} />
-        <Content>
-          <Header>{getRouteTitleFromLocation()}</Header>
-          <Main>
-            <Switch>
-              {routes.map((route, index) => (
-                <Route
-                  key={index}
-                  path={route.path}
-                  exact={route.exact}
-                  component={route.component}
-                />
-              ))}
-            </Switch>
-          </Main>
-        </Content>
-        <WelcomeModal
-          isOpen={showWelcomeModal}
-          onCancel={closeWelcomeModal}
-          onLoginComplete={closeWelcomeModal}
-          onSignupComplete={handleSignupComplete}
-        />
-      </Wrapper>
-    );
-  };
-
-  return renderApp();
+  return (
+    <Wrapper>
+      <SideBar onHelpClick={handleSideBarHelpClick} />
+      <Content>
+        <Header>{getRouteTitleFromLocation()}</Header>
+        <Main>
+          <Switch>
+            {routes.map((route, index) => (
+              <Route
+                key={index}
+                path={route.path}
+                exact={route.exact}
+                component={route.component}
+              />
+            ))}
+          </Switch>
+        </Main>
+      </Content>
+      <WelcomeModal
+        isOpen={showWelcomeModal}
+        onCancel={closeWelcomeModal}
+        onLoginComplete={closeWelcomeModal}
+        onSignupComplete={handleSignupComplete}
+      />
+    </Wrapper>
+  );
 };
 
 export default App;
