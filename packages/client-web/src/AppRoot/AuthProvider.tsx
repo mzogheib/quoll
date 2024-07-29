1b13f96140a22a0b439e69f59c3d9a4f52950468
Marwan Zogheib <mzogheib@users.noreply.github.com>
2024-07-30 08:32:44 +1000

    Add audience to auth0 provider

--- packages/client-web/src/AppRoot/AuthProvider.tsx
+++ packages/client-web/src/AppRoot/AuthProvider.tsx
@@ -16,6 +16,7 @@ const AuthProvider = ({ children }: Props) => (
     domain={process.env.REACT_APP_AUTH0_DOMAIN!}
     clientId={process.env.REACT_APP_AUTH0_CLIENT_ID!}
     authorizationParams={{
+      audience: process.env.REACT_APP_AUTH0_AUDIENCE,
       redirect_uri: window.location.origin,
     }}
     // These allow the session to be persisted across page refreshes
