5e6e875eba94c7155db39a6c111b47038be78da9
Marwan Zogheib <mzogheib@users.noreply.github.com>
2024-07-30 09:26:04 +1000

    Reset the auth user model on logout

--- packages/client-web/src/modules/auth/view-model/index.ts
+++ packages/client-web/src/modules/auth/view-model/index.ts
@@ -2,22 +2,24 @@ import { AuthModel, useAuthModel } from "../model";
 import { useTimelineModel } from "modules/timeline/model";
 import { useFeedsModel } from "modules/feeds/model";
 import { useDateModel } from "modules/date/model";
-import { useUserModel } from "modules/user/model";
+import { useAuthUserModel } from "modules/auth-user/model";
 
 type AuthViewModel = AuthModel;
 
 export const useAuthViewModel = (): AuthViewModel => {
+  const { getAccessToken } = useAuthModel();
+
   const authModel = useAuthModel();
   const timelineModel = useTimelineModel();
   const feedsModel = useFeedsModel();
   const dateModel = useDateModel();
-  const userModel = useUserModel();
+  const authUserModel = useAuthUserModel(getAccessToken);
 
   const _logout = async () => {
     timelineModel.reset();
     feedsModel.reset();
     dateModel.reset();
-    userModel.reset();
+    authUserModel.reset();
 
     return await authModel.logout();
   };
