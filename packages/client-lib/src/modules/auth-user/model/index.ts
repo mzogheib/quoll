44f5837f96a0200c8be7c0e7fd0cdf7d0aa94b8f
Marwan Zogheib <mzogheib@users.noreply.github.com>
2024-07-30 09:19:24 +1000

    Create a auth user model in client lib

---
+++ packages/client-lib/src/modules/auth-user/model/index.ts
@@ -0,0 +1,40 @@
+import { User } from "@quoll/lib";
+
+import { Store } from "../../../store/types";
+import { AuthUserService } from "../service";
+
+export type AuthUserState = {
+  isLoading: boolean;
+  user: User | null;
+};
+
+type AuthUserActions = {
+  getMe: (userId: string) => Promise<User>;
+  reset: () => void;
+};
+
+type AuthUserModel = AuthUserState & AuthUserActions;
+
+export const useAuthUserModel = (
+  useStore: () => Store<AuthUserState>,
+  service: AuthUserService,
+): AuthUserModel => {
+  const { state, setProperty, reset } = useStore();
+  const { user, isLoading } = state;
+
+  const getMe = async () => {
+    setProperty("isLoading", true);
+    const user = await service.getMe();
+    setProperty("user", user);
+    setProperty("isLoading", false);
+
+    return user;
+  };
+
+  return {
+    user,
+    isLoading,
+    getMe,
+    reset,
+  };
+};
