dd1603ed7670d6592a66d79351154b23189a33ae
Marwan Zogheib <mzogheib@users.noreply.github.com>
2024-08-05 08:34:49 +1000

    feat(client-web): enable v2 timeline

--- packages/client-web/src/modules/timeline/service/index.ts
+++ packages/client-web/src/modules/timeline/service/index.ts
@@ -1,13 +1,10 @@
 import { TimelineService } from "@quoll/client-lib";
 import { useMemo } from "react";
+import { getApiBaseUrl } from "services/api";
 
 export const useTimelineService = (getAccessToken: () => Promise<string>) => {
   const feedService = useMemo(() => {
-    if (process.env.REACT_APP_API_URL === undefined) {
-      throw new Error("REACT_APP_API_URL is not defined");
-    }
-
-    return new TimelineService(getAccessToken, process.env.REACT_APP_API_URL);
+    return new TimelineService(getAccessToken, getApiBaseUrl());
   }, [getAccessToken]);
 
   return feedService;
--- packages/client-web/src/modules/timeline/view-model/index.ts
+++ packages/client-web/src/modules/timeline/view-model/index.ts
@@ -1,6 +1,14 @@
 import { getAccessToken } from "services/session";
 import { useTimelineModel } from "../model";
+import { useAuthModel } from "modules/auth/model";
+import { checkIsFeatureEnabled } from "services/feature-flags";
 
 export const useTimelineViewModel = () => {
-  return useTimelineModel(getAccessToken);
+  const authModel = useAuthModel();
+
+  const _getAccessToken = checkIsFeatureEnabled("NEW_AUTH")
+    ? authModel.getAccessToken
+    : getAccessToken;
+
+  return useTimelineModel(_getAccessToken);
 };
