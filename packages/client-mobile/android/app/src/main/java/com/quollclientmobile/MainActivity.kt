69e32582e81bafd3c075d8734514f5e4ea90612e
Marwan Zogheib <mzogheib@users.noreply.github.com>
2024-08-11 07:55:36 +1000

    Rename all code and filenames

--- packages/client-mobile/android/app/src/main/java/com/quollclientmobile/MainActivity.kt
+++ packages/client-mobile/android/app/src/main/java/com/quollclientmobile/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.quollClientMobile
+package com.quollclientmobile
 
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
@@ -11,7 +11,7 @@ class MainActivity : ReactActivity() {
    * Returns the name of the main component registered from JavaScript. This is used to schedule
    * rendering of the component.
    */
-  override fun getMainComponentName(): String = "quollClientMobile"
+  override fun getMainComponentName(): String = "quollclientmobile"
 
   /**
    * Returns the instance of the [ReactActivityDelegate]. We use [DefaultReactActivityDelegate]
