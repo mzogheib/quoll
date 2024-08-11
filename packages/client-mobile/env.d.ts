dcb51399c08399651cddb702bb15df440d434015
Marwan Zogheib <mzogheib@users.noreply.github.com>
2024-08-11 18:30:52 +1000

    refactor(client-mobile): wrap app with Auth0 provider

--- packages/client-mobile/env.d.ts
+++ packages/client-mobile/env.d.ts
@@ -1,4 +1,6 @@
 declare module "@env" {
   export const API_URL: string;
   export const API_URL_ANDROID: string;
+  export const AUTH0_DOMAIN: string;
+  export const AUTH0_CLIENT_ID: string;
 }
--- packages/client-mobile/src/AppRoot/index.tsx
+++ packages/client-mobile/src/AppRoot/index.tsx
@@ -1,18 +1,22 @@
 import React from "react";
 import { SafeAreaView } from "react-native";
+import { Auth0Provider } from "react-native-auth0";
 import NavigationContainer from "@screens/NavigationContainer";
 
 import styles from "./styles";
 
 import App from "../App";
+import { AUTH0_DOMAIN, AUTH0_CLIENT_ID } from "@env";
 
 function AppRoot() {
   return (
-    <SafeAreaView style={styles.wrapper}>
-      <NavigationContainer>
-        <App />
-      </NavigationContainer>
-    </SafeAreaView>
+    <Auth0Provider domain={AUTH0_DOMAIN} clientId={AUTH0_CLIENT_ID}>
+      <SafeAreaView style={styles.wrapper}>
+        <NavigationContainer>
+          <App />
+        </NavigationContainer>
+      </SafeAreaView>
+    </Auth0Provider>
   );
 }
 
