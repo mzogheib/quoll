121cc6bb87d988a3a30170203b12c09f1592ddb0
Marwan Zogheib <mzogheib@users.noreply.github.com>
2024-08-12 20:55:57 +1000

    fix(client-mobile): clear timeline state on logout

--- packages/client-mobile/src/modules/auth/view-model/index.ts
+++ packages/client-mobile/src/modules/auth/view-model/index.ts
@@ -1,6 +1,7 @@
 import { AuthModel, useAuthModel } from "../model";
 import { useDateModel } from "@modules/date/model";
 import { useAuthUserModel } from "@modules/auth-user/model";
+import { useTimelineModel } from "@modules/timeline/model";
 
 type AuthViewModel = AuthModel;
 
@@ -9,10 +10,12 @@ export const useAuthViewModel = (): AuthViewModel => {
 
   const authModel = useAuthModel();
   const dateModel = useDateModel();
+  const timelineModel = useTimelineModel(getAccessToken);
   const authUserModel = useAuthUserModel(getAccessToken);
 
   const _logout = async () => {
     dateModel.reset();
+    timelineModel.reset();
     authUserModel.reset();
 
     return await authModel.logout();
