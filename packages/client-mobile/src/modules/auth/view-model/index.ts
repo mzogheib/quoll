3389e43535cf8956db820d9342068802d86fce2d
Marwan Zogheib <mzogheib@users.noreply.github.com>
2024-08-24 14:58:58 +1000

    refactor(client-mobile): change auth-user folder name to user

--- packages/client-mobile/src/modules/auth/view-model/index.ts
+++ packages/client-mobile/src/modules/auth/view-model/index.ts
@@ -1,7 +1,7 @@
 import { AuthModel } from "@quoll/client-lib/modules";
 import { useAuthModel } from "../model";
 import { useDateModel } from "@modules/date/model";
-import { useUserModel } from "@modules/auth-user/model";
+import { useUserModel } from "@modules/user/model";
 import { useTimelineModel } from "@modules/timeline/model";
 
 type AuthViewModel = AuthModel;
