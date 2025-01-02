f177e0a56be5fa2fa2367dd110e3f60f0a9c8463
Marwan Zogheib <mzogheib@users.noreply.github.com>
2025-01-02 15:05:37 +1100

    refactor(api): delete legacy code

--- packages/api/src/controllers/users.controller.ts
+++ packages/api/src/controllers/users.controller.ts
@@ -1,40 +1,5 @@
-import { FeedName, User } from "@quoll/lib/modules";
+import { FeedName } from "@quoll/lib/modules";
 import { IAuth, UserDoc, UserModel } from "../models/user.model";
-
-const DefaultFeeds = [
-  { name: "strava", auth: null },
-  { name: "toshl", auth: null },
-];
-
-function sanitizeUser(user: UserDoc): User {
-  const feeds = user.feeds.map((feed) => {
-    return {
-      name: feed.name,
-      isConnected: !!feed.auth,
-    };
-  });
-
-  return {
-    _id: user._id.toString(),
-    feeds,
-  };
-}
-
-export const createUser = async () => {
-  //  Not relevant here so we can make it up
-  const auth0Id = Math.random().toString(36).substring(2, 18);
-
-  const user = await UserModel.create({
-    auth0Id,
-    feeds: DefaultFeeds,
-  });
-  return sanitizeUser(user);
-};
-
-export const login = async (userId: string) => {
-  const user = await get(userId);
-  return sanitizeUser(user);
-};
 
 export const get = async (userId: string): Promise<UserDoc> => {
   const user = await UserModel.findById(userId);
--- packages/api/src/routes/index.ts
+++ packages/api/src/routes/index.ts
@@ -1,6 +1,5 @@
 import { Router } from "express";
 
-import { login, signup, authenticate } from "./users.route";
 import {
   checkAuth as checkFeedAuth,
   connect,
@@ -14,26 +13,6 @@ import { addUserIdMiddleware, createMeRoute, getMeRoute } from "./user.route";
 
 const router = Router();
 
-// v1 - legacy auth
-router.route("/login").post(login);
-router.route("/signup").post(signup);
-
-router
-  .route("/feed-auth")
-  .all(authenticate)
-  .get((req, res) => connect(req as RequestWithUserId, res))
-  .post((req, res) => authenticateFeed(req as RequestWithUserId, res))
-  // Only the deauthorize endpoint requires the feed to be authenticated
-  .all((req, res, next) => checkFeedAuth(req as RequestWithUserId, res, next))
-  .delete((req, res) => deauthorize(req as RequestWithUserId, res));
-
-router
-  .route("/timeline")
-  .all(authenticate)
-  .all((req, res, next) => checkFeedAuth(req as RequestWithUserId, res, next))
-  .get((req, res) => get(req as RequestWithUserId, res));
-
-// v2 - new auth
 router
   .route("/v2/user/me")
   .all(authMiddleware)
