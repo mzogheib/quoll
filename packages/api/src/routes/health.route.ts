728a0c6438efcc00990a07db8715971518c16663
Marwan Zogheib <mzogheib@users.noreply.github.com>
2025-02-11 07:53:16 +1100

    feat(api): add data to health response

--- packages/api/src/routes/health.route.ts
+++ packages/api/src/routes/health.route.ts
@@ -1,5 +1,8 @@
 import { Request, Response } from "express";
+import { getEnvVariable } from "../utils/env";
 
 export const getHealthRoute = async (_: Request, res: Response) => {
-  res.status(200).json({ status: "healthy" });
+  res
+    .status(200)
+    .json({ status: "healthy", data: getEnvVariable("HEALTH_DATA") });
 };
