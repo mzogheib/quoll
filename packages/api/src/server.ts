c18c5d85b53f6b1fc6f025d95bdc426bec5b98f0
Marwan Zogheib <mzogheib@users.noreply.github.com>
2025-06-01 10:59:51 +1000

    fix(api): wait for db connection attempt to finish before starting server

--- packages/api/src/server.ts
+++ packages/api/src/server.ts
@@ -6,24 +6,30 @@ import mongoose from "mongoose";
 import { getEnvVariable } from "./utils/env";
 import routes from "./routes";
 
-mongoose.connect(getEnvVariable("MONGODB_CONNECTION_STRING"));
+const init = async () => {
+  try {
+    await mongoose.connect(getEnvVariable("MONGODB_CONNECTION_STRING"));
+    console.log("DB connected successfully");
+  } catch (error) {
+    console.error("Error connecting to the database:", error);
+    process.exit(1);
+  }
 
-const db = mongoose.connection;
-if (!db) console.log("Error connecting DB");
-else console.log("DB connected successfully");
+  const app = express();
 
-const app = express();
+  app.set("port", getEnvVariable("PORT"));
 
-app.set("port", getEnvVariable("PORT"));
+  app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use(bodyParser.urlencoded({ extended: false }));
+  app.use(bodyParser.json());
 
-app.use(bodyParser.json());
+  app.use(cors({ origin: getEnvVariable("CORS_ALLOWED_ORIGIN") }));
 
-app.use(cors({ origin: getEnvVariable("CORS_ALLOWED_ORIGIN") }));
+  app.use("/api", routes);
 
-app.use("/api", routes);
+  const port = app.get("port");
 
-const port = app.get("port");
+  app.listen(port, () => console.log("Listening on port " + port));
+};
 
-app.listen(port, () => console.log("Listening on port " + port));
+init();
