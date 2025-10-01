feb7b4c8d46a32dce8a109c12e63d528cd3d6808
Marwan Zogheib <mzogheib@users.noreply.github.com>
2025-10-01 10:58:20 +1000

    Remove clustering implementation

--- packages/client-mobile/src/components/Map/index.tsx
+++ packages/client-mobile/src/components/Map/index.tsx
@@ -1,13 +1,11 @@
-import React, { useState } from "react";
+import React from "react";
 import MapView from "react-native-maps";
 
 import styles from "./styles";
 
-import { useClusters } from "@components/Map/clusters";
 import { useRegion } from "@components/Map/region";
 import { useGeolocationViewModel } from "@modules/geolocation/view-model";
 import Marker from "./Marker";
-import ClusterMarker from "./ClusterMarker";
 import { MarkerProps } from "./types";
 import { OriginalAspectRatioImage } from "@components/OriginalAspectRatioImage";
 
@@ -18,57 +16,31 @@ type Props = {
 
 export const Map = ({ markers, onMarkerPress }: Props) => {
   const { isConnected } = useGeolocationViewModel();
-  const { region: initialRegion } = useRegion({ markers });
-  const [region, setRegion] = useState(initialRegion);
-  const { clusters } = useClusters({ markers, region });
+  const { region } = useRegion({ markers });
 
   return (
     <MapView
       style={styles.wrapper}
-      region={initialRegion}
+      region={region}
       showsUserLocation={isConnected}
       onPress={() => onMarkerPress(null)}
-      onRegionChangeComplete={setRegion}
     >
-      {clusters?.map((cluster) => {
-        const [longitude, latitude] = cluster.geometry.coordinates;
-        const {
-          cluster: isCluster,
-          cluster_id,
-          point_count,
-          markerId,
-          image,
-          isSelected,
-        } = cluster.properties;
-
-        if (isCluster) {
-          return (
-            <ClusterMarker
-              key={cluster_id}
-              count={point_count}
-              coordinate={{ latitude, longitude }}
-            />
-          );
-        }
-
+      {markers?.map(({ coordinate, id, image, isSelected }) => {
+        const { longitude, latitude } = coordinate;
         const renderCallout = image
           ? () => <OriginalAspectRatioImage source={image} width={325} />
           : undefined;
-        const markerChildren = image ? (
-          <OriginalAspectRatioImage source={image} width={40} height={40} />
-        ) : null;
 
         return (
           <Marker
-            key={markerId}
-            id={markerId}
-            shouldShowCallout={isSelected}
+            key={id}
+            id={id}
             coordinate={{ latitude, longitude }}
-            onPress={() => onMarkerPress(markerId)}
+            onPress={() => onMarkerPress(id)}
+            shouldShowCallout={isSelected}
+            children={undefined}
             renderCallout={renderCallout}
-          >
-            {markerChildren}
-          </Marker>
+          />
         );
       })}
     </MapView>
