5ab29cd06db99c0103eba94a9d4d8bfb8a69a9e5
Marwan Zogheib <mzogheib@users.noreply.github.com>
2025-10-02 09:39:52 +1000

    Change entry detail position

--- packages/client-mobile/src/components/EntryDetail/index.tsx
+++ packages/client-mobile/src/components/EntryDetail/index.tsx
@@ -20,7 +20,7 @@ const EntryDetail = ({ entry }: Props) => {
         <OriginalAspectRatioImage
           source={{ uri: mediaUri }}
           width={contentWidth}
-          maxHeight={400}
+          maxHeight={200}
         />
       )}
       {description && (
