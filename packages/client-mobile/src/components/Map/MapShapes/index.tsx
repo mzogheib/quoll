3c08d428fd86acbbdbc28d822e33750a98082aac
Marwan Zogheib <mzogheib@users.noreply.github.com>
2025-10-08 08:25:09 +1100

    Add comments

--- packages/client-mobile/src/components/Map/MapShapes/index.tsx
+++ packages/client-mobile/src/components/Map/MapShapes/index.tsx
@@ -8,10 +8,16 @@ import { MarkerProps } from "../types";
 import { FeatureProperties, isMarkerProperties } from "./types";
 
 type Props = {
+  /** The markers to display on the map. */
   markers: MarkerProps[];
+  /** Called when a marker is pressed. */
   onMarkerPress: (id: string) => void;
 };
 
+/**
+ * Displays the given markers on the map, clustering them when they are close
+ * to each other.
+ */
 export const MapShapes = ({ markers, onMarkerPress }: Props) => {
   const featureCollection: GeoJSON.FeatureCollection<
     GeoJSON.Geometry,
