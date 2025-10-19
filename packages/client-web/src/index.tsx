81976f27b36073cfe0e093a7fc2f8a7fb885b17b
Marwan Zogheib <mzogheib@users.noreply.github.com>
2025-10-20 09:08:24 +1100

    Add URL polyfill

--- packages/client-web/src/index.tsx
+++ packages/client-web/src/index.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
+import "react-native-url-polyfill/auto";
 
 import AppRoot from "./AppRoot";
 import reportWebVitals from "./reportWebVitals";
